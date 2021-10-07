623d5092c91952a5423be7d298ff432bf7d26ae1
Michael Forster <forster@google.com>
2021-10-07 13:38:27 +0000

    Move the type mapping unit test to Rust.
    PiperOrigin-RevId: 401497197

--- rs_bindings_from_cc/ir_from_cc_test.rs
+++ rs_bindings_from_cc/ir_from_cc_test.rs
@@ -102,3 +102,106 @@ fn test_doc_comment() -> Result<()> {
 
     Ok(())
 }
+
+#[test]
+fn test_type_conversion() -> Result<()> {
+    let ir = ir_from_cc(
+        r#"
+            #include <stdint.h>
+            #include <stddef.h>
+
+            struct S {
+                bool b;
+
+                char c;
+                unsigned char uc;
+                signed char sc;
+                char16_t c16;
+                char32_t c32;
+                wchar_t wc;
+
+                short s;
+                int i;
+                long l;
+                long long ll;
+
+                unsigned short us;
+                unsigned int ui;
+                unsigned long ul;
+                unsigned long long ull;
+
+                signed short ss;
+                signed int si;
+                signed long sl;
+                signed long long sll;
+
+                int8_t i8;
+                int16_t i16;
+                int32_t i32;
+                int64_t i64;
+
+                uint8_t u8;
+                uint16_t u16;
+                uint32_t u32;
+                uint64_t u64;
+
+                ptrdiff_t pt;
+                size_t st;
+                intptr_t ip;
+                uintptr_t up;
+
+                float f;
+                double d;
+            };
+        "#,
+    )?;
+    let fields = ir.records().next().unwrap().fields.iter();
+    let type_mapping: HashMap<_, _> =
+        fields.map(|f| (f.type_.cc_type.name.as_str(), f.type_.rs_type.name.as_str())).collect();
+
+    assert_eq!(type_mapping["bool"], "bool");
+
+    assert_eq!(type_mapping["char"], "i8");
+    assert_eq!(type_mapping["unsigned char"], "u8");
+    assert_eq!(type_mapping["signed char"], "i8");
+    assert_eq!(type_mapping["char16_t"], "u16");
+    // We cannot map C++ char32_t or wchar_t to Rust char,
+    // because Rust requires that chars are valid UTF scalar values.
+    assert_eq!(type_mapping["char32_t"], "u32");
+    assert_eq!(type_mapping["wchar_t"], "i32");
+
+    assert_eq!(type_mapping["short"], "i16");
+    assert_eq!(type_mapping["int"], "i32");
+    assert_eq!(type_mapping["long"], "i64");
+    assert_eq!(type_mapping["long long"], "i64");
+
+    assert_eq!(type_mapping["unsigned short"], "u16");
+    assert_eq!(type_mapping["unsigned int"], "u32");
+    assert_eq!(type_mapping["unsigned long"], "u64");
+    assert_eq!(type_mapping["unsigned long long"], "u64");
+
+    assert_eq!(type_mapping["short"], "i16");
+    assert_eq!(type_mapping["int"], "i32");
+    assert_eq!(type_mapping["long"], "i64");
+    assert_eq!(type_mapping["long long"], "i64");
+
+    assert_eq!(type_mapping["int8_t"], "i8");
+    assert_eq!(type_mapping["int16_t"], "i16");
+    assert_eq!(type_mapping["int32_t"], "i32");
+    assert_eq!(type_mapping["int64_t"], "i64");
+
+    assert_eq!(type_mapping["uint8_t"], "u8");
+    assert_eq!(type_mapping["uint16_t"], "u16");
+    assert_eq!(type_mapping["uint32_t"], "u32");
+    assert_eq!(type_mapping["uint64_t"], "u64");
+
+    assert_eq!(type_mapping["ptrdiff_t"], "isize");
+    assert_eq!(type_mapping["size_t"], "usize");
+    assert_eq!(type_mapping["intptr_t"], "isize");
+    assert_eq!(type_mapping["uintptr_t"], "usize");
+
+    assert_eq!(type_mapping["float"], "f32");
+    assert_eq!(type_mapping["double"], "f64");
+
+    Ok(())
+}
