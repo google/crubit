3cda505386b696a7bdc4034583bbe4c80ac593aa
Marcel Hlopko <hlopko@google.com>
2021-11-05 15:21:20 +0000

    Pass target and header information to the rs_bindings_from_cc tool during the build
    In this CL we add --targets_and_headers flag, currently noop, that contains
    following data in JSON format:
    
    [
      {"t": "//foo/bar:baz", "h": ["foo/bar/baz/header1.h", "foo/bar/baz/header2.h"]},
      {"t": "//base:base", "h": ["base/header1.h", "base/header2.h"]},
      ...
    ]
    
    JSON was chosen for following reasons:
    
    * absl flags don't natively support repeated flags that accumulate result (in
      other words, if one flag is present multiple times on the command line, the
      last one wins unconditionally).
    * if we want to emulate repeated flags, we have to encode the format, or
      separators, somehow.[]
      allowed as target name, so we have 2 options:
      1) we either escape special characters, and if we have to go that way, let's
         just admit defeat and use JSON. This is what we do here.
      2) we use uncommon combinations (for example tripple semicolon as line
         separator, and tripple comma as a column separator. We detect these patterns
         and we fail the build on a conflict.
    * not currently the case, but there is a potential for performance optimization
      in blaze - we have to generate JSON from a depset, and Blaze could cache
      generated JSON snippets from transitive depsets.
    
    I'm happy to choose 2) (because it only occured to me while writing the CL
    description, and the 1) was already implemented :)
    
    This design can also be revisited once/if we move from cc_binary to rust_binary, and using a different flags library than absl.
    
    Actual logic that uses the flag in the tool will come as a followup.
    
    PiperOrigin-RevId: 407826722

--- rs_bindings_from_cc/rs_bindings_from_cc.cc
+++ rs_bindings_from_cc/rs_bindings_from_cc.cc
@@ -17,6 +17,7 @@
 #include "file/base/filesystem.h"
 #include "file/base/helpers.h"
 #include "file/base/options.h"
+#include "third_party/absl/container/flat_hash_map.h"
 #include "third_party/absl/flags/flag.h"
 #include "third_party/absl/status/status.h"
 #include "third_party/absl/status/statusor.h"
@@ -35,6 +36,16 @@ ABSL_FLAG(std::vector<std::string>, public_headers, std::vector<std::string>(),
           "public headers of the cc_library this tool should generate bindings "
           "for, in a format suitable for usage in google3-relative quote "
           "include (#include \"\").");
+ABSL_FLAG(std::string, targets_and_headers, std::string(),
+          "Information about which headers belong to which targets, encoded as "
+          "a JSON array. For example: "
+          "[\n"
+          "  {\n"
+          "     \"t\": \"//foo/bar:baz\",\n"
+          "     \"h\": [\"foo/bar/header1.h\", \"foo/bar/header2.h\"]\n"
+          "  },\n"
+          "...\n"
+          "]");
 
 int main(int argc, char* argv[]) {
   InitGoogle(argv[0], &argc, &argv, true);
@@ -46,6 +57,24 @@ int main(int argc, char* argv[]) {
   auto public_headers = absl::GetFlag(FLAGS_public_headers);
   QCHECK(!public_headers.empty())
       << "please specify at least one header in --public_headers";
+  auto targets_and_headers_json = absl::GetFlag(FLAGS_targets_and_headers);
+  if (!targets_and_headers_json.empty()) {
+    nlohmann::json targets_and_headers =
+        nlohmann::json::parse(targets_and_headers_json);
+    absl::flat_hash_map<std::string, std::string> headers_to_targets;
+    QCHECK(targets_and_headers.is_array())
+        << "Expected `--targets_and_headers` to be a Json array of objects";
+    for (const auto& target_and_headers : targets_and_headers) {
+      std::string target = target_and_headers["t"];
+      CHECK(target_and_headers["h"].is_array())
+          << "Expected `h` fields of `--targets_and_headers` "
+             "to be an array of strings";
+      for (std::string header : target_and_headers["h"]) {
+        headers_to_targets.insert(
+            std::pair<std::string, std::string>(header, target));
+      }
+    }
+  }
 
   auto ir_out = absl::GetFlag(FLAGS_ir_out);  // Optional.
 
