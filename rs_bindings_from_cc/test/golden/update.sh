fc16b8fb149bedc69d20925fed250f216f5b27a6
Marcel Hlopko <hlopko@google.com>
2021-11-30 10:33:37 +0000

    Fix update.sh after the change to use aspect
    PiperOrigin-RevId: 413096589

--- rs_bindings_from_cc/test/golden/update.sh
+++ rs_bindings_from_cc/test/golden/update.sh
@@ -25,6 +25,6 @@ done
 bazel build "${TARGETS[@]}"
 
 for header in *.h; do
-  cat "$(bazel info bazel-bin)/${PKG}/${header%.h}_bindings_files_rs_api.rs" >"${header%.h}_rs_api.rs"
-  cat "$(bazel info bazel-bin)/${PKG}/${header%.h}_bindings_files_rs_api_impl.cc" >"${header%.h}_rs_api_impl.cc"
+  cat "$(bazel info bazel-bin)/${PKG}/${header%.h}_cc_rust_api.rs" >"${header%.h}_rs_api.rs"
+  cat "$(bazel info bazel-bin)/${PKG}/${header%.h}_cc_rust_api_impl.cc" >"${header%.h}_rs_api_impl.cc"
 done
