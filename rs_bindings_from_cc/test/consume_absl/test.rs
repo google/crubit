14799f283639b55c0edd32c8750dfa1e2c5ec7f0
Googler <no-reply@google.com>
2022-01-04 13:55:16 +0000

    Add an end-to-end test for consuming simple Abseil types and functions.
    The test is deactivated as it doesn't actually work yet, but the I think it's a
    useful example of the relatively simple cases that we want to get to work. The
    idea is that the test can be activated locally to see how far we currently get
    as we work towards enabling everything we need for the test.
    
    PiperOrigin-RevId: 419574129

---
+++ rs_bindings_from_cc/test/consume_absl/test.rs
@@ -0,0 +1,17 @@
+// Part of the Crubit project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#[cfg(test)]
+mod tests {
+    // TODO(mboehme): Disabled because absl::Duration cannot yet be imported.
+    /*
+    #[test]
+    fn test_seconds() {
+        use time::Seconds;
+        use time::ToInt64Seconds;
+        let d = Seconds(42);
+        assert_eq!(ToInt64Seconds(d), 42);
+    }
+    */
+}
