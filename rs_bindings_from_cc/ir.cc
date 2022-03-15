7039390d59706a2118bb636ba66bfa1554186feb
Devin Jeanpierre <jeanpierreda@google.com>
2022-03-15 13:22:59 +0000

    Merge the logic of PointerTo and ReferenceTo.
    These only differ slightly: ReferenceTo doesn't allow for null, and they have different cc_name values. Unfortunately, that last difference means it doesn't really make sense for ReferenceTo to directly call PointerTo. But they do basically do the same thing: in crubit IR, both pointers and references are a bit interchangable.
    
    PiperOrigin-RevId: 434733085

--- rs_bindings_from_cc/ir.cc
+++ rs_bindings_from_cc/ir.cc
@@ -77,11 +77,12 @@ nlohmann::json CcType::ToJson() const {
   return result;
 }
 
-MappedType MappedType::PointerTo(MappedType pointee_type,
-                                 std::optional<LifetimeId> lifetime,
-                                 bool nullable) {
-  absl::string_view rs_name;
+static MappedType PointerOrReferenceTo(MappedType pointee_type,
+                                       absl::string_view cc_ptr_name,
+                                       std::optional<LifetimeId> lifetime,
+                                       bool nullable) {
   bool has_lifetime = lifetime.has_value();
+  absl::string_view rs_name;
   if (has_lifetime) {
     rs_name = pointee_type.cc_type.is_const ? internal::kRustRefConst
                                             : internal::kRustRefMut;
@@ -90,7 +91,7 @@ MappedType MappedType::PointerTo(MappedType pointee_type,
                                             : internal::kRustPtrMut;
   }
   auto pointer_type =
-      Simple(std::string(rs_name), std::string(internal::kCcPtr));
+      MappedType::Simple(std::string(rs_name), std::string(cc_ptr_name));
   if (has_lifetime) {
     pointer_type.rs_type.lifetime_args.push_back(*std::move(lifetime));
   }
@@ -103,24 +104,17 @@ MappedType MappedType::PointerTo(MappedType pointee_type,
   return pointer_type;
 }
 
+MappedType MappedType::PointerTo(MappedType pointee_type,
+                                 std::optional<LifetimeId> lifetime,
+                                 bool nullable) {
+  return PointerOrReferenceTo(std::move(pointee_type), internal::kCcPtr,
+                              lifetime, nullable);
+}
+
 MappedType MappedType::LValueReferenceTo(MappedType pointee_type,
                                          std::optional<LifetimeId> lifetime) {
-  absl::string_view rs_name;
-  if (lifetime.has_value()) {
-    rs_name = pointee_type.cc_type.is_const ? internal::kRustRefConst
-                                            : internal::kRustRefMut;
-  } else {
-    rs_name = pointee_type.cc_type.is_const ? internal::kRustPtrConst
-                                            : internal::kRustPtrMut;
-  }
-  auto reference_type =
-      Simple(std::string(rs_name), std::string(internal::kCcLValueRef));
-  if (lifetime.has_value()) {
-    reference_type.rs_type.lifetime_args.push_back(*std::move(lifetime));
-  }
-  reference_type.rs_type.type_args.push_back(std::move(pointee_type.rs_type));
-  reference_type.cc_type.type_args.push_back(std::move(pointee_type.cc_type));
-  return reference_type;
+  return PointerOrReferenceTo(std::move(pointee_type), internal::kCcLValueRef,
+                              lifetime, /*nullable=*/false);
 }
 
 MappedType MappedType::FuncPtr(absl::string_view cc_call_conv,
