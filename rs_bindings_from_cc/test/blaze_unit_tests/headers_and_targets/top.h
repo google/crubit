47505377c1b43ad3cc26547e0a43c4430990f438
Marcel Hlopko <hlopko@google.com>
2022-03-24 11:54:41 +0000

    internal change
    PiperOrigin-RevId: 436964860

--- rs_bindings_from_cc/test/blaze_unit_tests/headers_and_targets/top.h
+++
@@ -1,8 +0,0 @@
-// Part of the Crubit project, under the Apache License v2.0 with LLVM
-// Exceptions. See /LICENSE for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-
-#ifndef CRUBIT_RS_BINDINGS_FROM_CC_TEST_BLAZE_UNIT_TESTS_HEADERS_AND_TARGETS_TOP_H_
-#define CRUBIT_RS_BINDINGS_FROM_CC_TEST_BLAZE_UNIT_TESTS_HEADERS_AND_TARGETS_TOP_H_
-
-#endif  // CRUBIT_RS_BINDINGS_FROM_CC_TEST_BLAZE_UNIT_TESTS_HEADERS_AND_TARGETS_TOP_H_
