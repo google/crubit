a4d33d99c9f850ad988d9c5043285bddb9d4042d
Marcel Hlopko <hlopko@google.com>
2022-05-04 07:57:17 -0700

    Compile golden files
    We used to do this in the past, but then probably sometimes during changes to
    the Bazel infrastructure we stopped. This CL brings this back in the form of an
    empty `rust_test` that triggers the compilation of all bindings, but doesn't
    test anything at runtime.
    
    PiperOrigin-RevId: 446457767

---
+++ rs_bindings_from_cc/test/golden/empty_rs_test.rs
@@ -0,0 +1,7 @@
+// Part of the Crubit project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+// The purpose of this `rust_test` is to trigger compilation of the bindings
+// this targets depend on. Therefore this file is intentionally left empty, what
+// matters is `deps` attribute of the `rust_test` target in the BUILD file.
