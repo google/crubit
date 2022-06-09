google/crubit

commit 14732b2920053fadd06917ffd9201cd6d64704ff

Support for template specializations used outside of type aliases.
Before this CL, Crubit would generate bindings for template
instantiations used in type aliases: `using MyTypeAlias =
MyTemplate<int>`.  Template instantiations used elsewhere would
result in emitting an `UnsupportedItem`.

This CL moves that call to `Importer::ConvertTemplateSpecializationType`
from `TypedefNameDeclImporter::Import` to `Importer::ConvertType`.
After this CL, Crubit supports template specializations used in such
places as 1) function return types, 2) function parameter types, 3)
private and public fields of structs, etc.

PiperOrigin-RevId: 452551994

rs_bindings_from_cc/importer.cc
@@ -636,7 +636,7 @@ absl::StatusOr<MappedType> Importer::ConvertTypeDecl(
 absl::StatusOr<MappedType> Importer::ConvertType(
     const clang::Type* type,
     std::optional<clang::tidy::lifetimes::ValueLifetimes>& lifetimes,
-    bool nullable) const {
+    bool nullable) {
   // Qualifiers are handled separately in ConvertQualType().
   std::string type_string = clang::QualType(type, 0).getAsString();
 
@@ -741,6 +741,9 @@ absl::StatusOr<MappedType> Importer::ConvertType(
   } else if (const auto* typedef_type =
                  type->getAsAdjusted<clang::TypedefType>()) {
     return ConvertTypeDecl(typedef_type->getDecl());
+  } else if (const auto* tst_type =
+                 type->getAs<clang::TemplateSpecializationType>()) {
+    return ConvertTemplateSpecializationType(tst_type);
   } else if (const auto* subst_type =
                  type->getAs<clang::SubstTemplateTypeParmType>()) {
     return ConvertQualType(subst_type->getReplacementType(), lifetimes);
@@ -758,7 +761,7 @@ absl::StatusOr<MappedType> Importer::ConvertType(
 absl::StatusOr<MappedType> Importer::ConvertQualType(
     clang::QualType qual_type,
     std::optional<clang::tidy::lifetimes::ValueLifetimes>& lifetimes,
-    bool nullable) const {
+    bool nullable) {
   std::string type_string = qual_type.getAsString();
   absl::StatusOr<MappedType> type =
       ConvertType(qual_type.getTypePtr(), lifetimes, nullable);

rs_bindings_from_cc/test/templates/func_return_and_param_types/BUILD
@@ -1,0 +1,17 @@
+"""End-to-end example of using fully-instantiated templates as function return types."""
+
+load("@rules_rust//rust:defs.bzl", "rust_test")
+
+licenses(["notice"])
+
+cc_library(
+    name = "func_return_and_param_types",
+    srcs = ["func_return_and_param_types.cc"],
+    hdrs = ["func_return_and_param_types.h"],
+)
+
+rust_test(
+    name = "main",
+    srcs = ["test.rs"],
+    cc_deps = [":func_return_and_param_types"],
+)

rs_bindings_from_cc/test/templates/func_return_and_param_types/test.rs
@@ -1,0 +1,25 @@
+// Part of the Crubit project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#[cfg(test)]
+mod tests {
+    use func_return_and_param_types::*;
+
+    // This tests whether Crubit supports template specialization/instantiation in a
+    // function return type, or in a function parameter type - see b/228868369.
+    #[test]
+    fn test_template_instantiation_in_return_value_and_parameter_type() {
+        // Note that the Rust code below never needs to refer to the
+        // mangled name of the Rust struct that the class template
+        // specialization/instantiation gets translated to.
+
+        // Class template instantiation used as a function return type.
+        let s = CreateInstanceOfMyTemplate(123);
+        assert_eq!(123, *s.value());
+
+        // Const-ref to class template instantiation used as a function parameter type.
+        let d = DoubleInstanceOfMyTemplate(&s);
+        assert_eq!(123 * 2, d);
+    }
+}

rs_bindings_from_cc/test/templates/struct_fields/BUILD
@@ -1,0 +1,17 @@
+"""End-to-end example of using fully-instantiated templates as function return types."""
+
+load("@rules_rust//rust:defs.bzl", "rust_test")
+
+licenses(["notice"])
+
+cc_library(
+    name = "struct_fields",
+    hdrs = ["struct_fields.h"],
+)
+
+rust_test(
+    name = "main",
+    srcs = ["test.rs"],
+    cc_deps = [":struct_fields"],
+    deps = ["//rs_bindings_from_cc/support:ctor"],
+)

rs_bindings_from_cc/test/templates/struct_fields/test.rs
@@ -1,0 +1,24 @@
+// Part of the Crubit project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#[cfg(test)]
+mod tests {
+    use ctor::CtorNew as _;
+    use struct_fields::*;
+
+    // This tests whether Crubit supports template specialization/instantiation in a
+    // struct field - see b/228868369.
+    #[test]
+    fn test_template_instantiation_in_return_value_and_parameter_type() {
+        // Note that the Rust code below never needs to refer to the
+        // mangled name of the Rust struct that the class template
+        // specialization/instantiation gets translated to.
+
+        // Class template instantiation used as a type of a public field.
+        ctor::emplace! {
+            let s = MyStruct::ctor_new(123);
+        }
+        assert_eq!(123, *s.public_field.value());
+    }
+}

rs_bindings_from_cc/decl_importer.h
@@ -136,12 +136,7 @@ class ImportContext {
   virtual absl::StatusOr<MappedType> ConvertQualType(
       clang::QualType qual_type,
       std::optional<clang::tidy::lifetimes::ValueLifetimes>& lifetimes,
-      bool nullable = true) const = 0;
-
-  // Converts `type` into a MappedType, after first importing the Record behind
-  // the template instantiation.
-  virtual absl::StatusOr<MappedType> ConvertTemplateSpecializationType(
-      const clang::TemplateSpecializationType* type) = 0;
+      bool nullable = true) = 0;
 
   // Marks `decl` as successfully imported.  Other pieces of code can check
   // HasBeenAlreadySuccessfullyImported to avoid introducing dangling ItemIds

rs_bindings_from_cc/importer.h
@@ -76,9 +76,7 @@ class Importer : public ImportContext {
   absl::StatusOr<MappedType> ConvertQualType(
       clang::QualType qual_type,
       std::optional<clang::tidy::lifetimes::ValueLifetimes>& lifetimes,
-      bool nullable = true) const override;
-  absl::StatusOr<MappedType> ConvertTemplateSpecializationType(
-      const clang::TemplateSpecializationType* type) override;
+      bool nullable = true) override;
   void MarkAsSuccessfullyImported(const clang::TypeDecl* decl) override;
   bool HasBeenAlreadySuccessfullyImported(
       const clang::TypeDecl* decl) const override;
@@ -102,8 +100,13 @@ class Importer : public ImportContext {
   absl::StatusOr<MappedType> ConvertType(
       const clang::Type* type,
       std::optional<clang::tidy::lifetimes::ValueLifetimes>& lifetimes,
-      bool nullable) const;
+      bool nullable);
   absl::StatusOr<MappedType> ConvertTypeDecl(const clang::TypeDecl* decl) const;
+
+  // Converts `type` into a MappedType, after first importing the Record behind
+  // the template instantiation.
+  absl::StatusOr<MappedType> ConvertTemplateSpecializationType(
+      const clang::TemplateSpecializationType* type);
 
   std::vector<std::unique_ptr<DeclImporter>> decl_importers_;
   std::unique_ptr<clang::MangleContext> mangler_;

rs_bindings_from_cc/importers/typedef_name.cc
@@ -32,17 +32,9 @@ std::optional<IR::Item> crubit::TypedefNameDeclImporter::Import(
       ictx_.GetTranslatedIdentifier(typedef_name_decl);
   CRUBIT_CHECK(identifier.has_value());  // This should never happen.
 
-  absl::StatusOr<MappedType> underlying_type;
-  // TODO(b/228868369): Move this "if" into the generic TypeMapper::ConvertType.
-  // This will extend support for template instantiations outside type aliases.
-  if (const auto* tst_type = typedef_name_decl->getUnderlyingType()
-                                 ->getAs<clang::TemplateSpecializationType>()) {
-    underlying_type = ictx_.ConvertTemplateSpecializationType(tst_type);
-  } else {
-    std::optional<clang::tidy::lifetimes::ValueLifetimes> no_lifetimes;
-    underlying_type = ictx_.ConvertQualType(
-        typedef_name_decl->getUnderlyingType(), no_lifetimes);
-  }
+  std::optional<clang::tidy::lifetimes::ValueLifetimes> no_lifetimes;
+  absl::StatusOr<MappedType> underlying_type = ictx_.ConvertQualType(
+      typedef_name_decl->getUnderlyingType(), no_lifetimes);
   if (underlying_type.ok()) {
     if (const auto* tag_decl = type->getAsTagDecl();
         tag_decl && tag_decl->getDeclContext() == decl_context &&

commit 3a2026b39ae3b7d6cceef25b8c19a1708f19918c

Fixing infinite recursion in ConvertTemplateSpecializationType.
Before this CL, a class template with a member function that
refers back to the same class ...

```cpp
    template <typename T>
    struct MyTemplate {
      void MyMethod(const MyTemplate<T>& other) const {}
    };
    using MyTypeAlias = MyTemplate<int>;
```

... would cause an infinite recursion:

```txt
    ...
    Importer::ConvertTemplateSpecializationType() <- importing MyTemplate<int>
    Importer::ConvertType() at importer.cc:746:12
    Importer::ConvertQualType() at importer.cc:767:7
    Importer::ConvertType() at importer.cc:694:5
    Importer::ConvertQualType() at importer.cc:767:7
    FunctionDeclImporter::Import() at function.cc:69:29 <- importing MyMethod
    DeclImporterBase<clang::FunctionDecl>::ImportDecl() ...
    Importer::ImportDecl() at importer.cc:461:26
    Importer::GetDeclItem() at importer.cc:452:38
    Importer::GetItemIdsInSourceOrder() at importer.cc:331:9
    CXXRecordDeclImporter::Import() at cxx_record.cc:137:25
    DeclImporterBase<clang::CXXRecordDecl>::ImportDecl() ...
    Importer::ImportDecl() at importer.cc:461:26
    Importer::GetDeclItem() at importer.cc:452:38
    Importer::ConvertTemplateSpecializationType() <- importing MyTemplate<int>
    ...
```

The problem above was introduced in https://github.com/google/crubit/commit/14732b2920053fadd06917ffd9201cd6d64704ff which started to call
ConvertTemplateSpecializationType from ConvertType (rather than from
TypedefNameDeclImporter::Import which won't be recursively reentered
in this scenario).

This CL prevents the infinite recursion by checking `known_types_` to
check if the `ClassTemplateSpecializationDecl` has been already
processed before.

Interestingly, I've wondered about the infinite recursion before, but
dismissed this problem, because I thought that it is sufficiently tested
via `this` parameter of instance methods (which already had some test

rs_bindings_from_cc/importer.cc
@@ -595,6 +595,9 @@ absl::StatusOr<MappedType> Importer::ConvertTemplateSpecializationType(
         type_string));
   }
 
+  if (HasBeenAlreadySuccessfullyImported(specialization_decl))
+    return ConvertTypeDecl(specialization_decl);
+
   if (IsFromCurrentTarget(specialization_decl) &&
       !specialization_decl->isExplicitSpecialization()) {
     // Store implicit `specialization_decl`s so that they will get included in

rs_bindings_from_cc/test/templates/method_params/BUILD
@@ -1,0 +1,16 @@
+"""End-to-end example of using type aliases to fully-instantiated templates."""
+
+load("@rules_rust//rust:defs.bzl", "rust_test")
+
+licenses(["notice"])
+
+cc_library(
+    name = "method_params",
+    hdrs = ["method_params.h"],
+)
+
+rust_test(
+    name = "main",
+    srcs = ["test.rs"],
+    cc_deps = [":method_params"],
+)

rs_bindings_from_cc/test/templates/method_params/method_params.h
@@ -1,0 +1,34 @@
+// Part of the Crubit project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#ifndef THIRD_PARTY_CRUBIT_RS_BINDINGS_FROM_CC_TEST_TEMPLATES_METHOD_PARAMS_METHOD_PARAMS_H_
+#define THIRD_PARTY_CRUBIT_RS_BINDINGS_FROM_CC_TEST_TEMPLATES_METHOD_PARAMS_METHOD_PARAMS_H_
+
+#pragma clang lifetime_elision
+
+template <typename T>
+class MyTemplate {
+ public:
+  static MyTemplate Create(T value) {
+    MyTemplate result;
+    result.value_ = value;
+    return result;
+  }
+
+  T AddOneOtherItem(const MyTemplate<T>& other) const {
+    return value_ + other.value_;
+  }
+
+  T AddThreeOtherItems(const MyTemplate<T>& a, const MyTemplate<T>& b,
+                       const MyTemplate<T>& c) const {
+    return value_ + a.value_ + b.value_ + c.value_;
+  }
+
+ private:
+  T value_;
+};
+
+using MyTypeAlias = MyTemplate<int>;
+
+#endif  // THIRD_PARTY_CRUBIT_RS_BINDINGS_FROM_CC_TEST_TEMPLATES_METHOD_PARAMS_METHOD_PARAMS_H_

rs_bindings_from_cc/test/templates/method_params/test.rs
@@ -1,0 +1,31 @@
+// Part of the Crubit project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_const_ref_to_self() {
+        // Among other things, this test provides coverage against infinite
+        // recursion around Importer::ConvertTemplateSpecializationType which
+        // in the past might have happened when a member function's parameters
+        // would refer back to the same class template specialization.
+        let s1 = method_params::MyTypeAlias::Create(1);
+        let s2 = method_params::MyTypeAlias::Create(2);
+        assert_eq!(1 + 2, s1.AddOneOtherItem(&s2));
+    }
+
+    fn test_repeating_parameter_type() {
+        // Among other things, this test provides coverage for the (not currently
+        // implemented, but still considered for the future) mangling algorithm
+        // that is required as part of the "Function template" approach from
+        // `thunks_for_class_template_member_functions.md`. In particular,
+        // repeated parameter type should be subject to https://itanium-cxx-abi.github.io/cxx-abi/abi.html#mangling-compression
+        // (which was missed in the initial prototype of this approach).
+        let s1 = method_params::MyTypeAlias::Create(1);
+        let s2 = method_params::MyTypeAlias::Create(2);
+        let s3 = method_params::MyTypeAlias::Create(3);
+        let s4 = method_params::MyTypeAlias::Create(4);
+        assert_eq!(1 + 2 + 3 + 4, s1.AddThreeOtherItems(&s2, &s3, &s4));
+    }
+}