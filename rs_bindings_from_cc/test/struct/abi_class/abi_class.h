215cf78c9277d36ee28bd1e5f9fee376e6326eab
Lukasz Anforowicz <lukasza@google.com>
2023-02-23 11:16:33 -0800

    Regression tests of ABI incompatibility in a `StructFloat`.
    The failure of the `test_struct_float` test introduced by this CL shows
    that `rs_bindings_from_cc` may accidentally generate Rust structs that
    are layout-compatible, but ABI-incompatible with C++ structs.  This is
    most likely caused by replacing some struct fields with an opaque blob
    of bytes (which may have different ABI classification than the original
    fields).
    
    The test is temporarily disabled via `#[ignore]` - one needs to use the
    `--include-ignored` cmdline argument to run the test:
    
    ```
    $ bazel test --test_output=streamed \
        rs_bindings_from_cc/test/struct/abi_class:main \
        --test_arg=test_struct_float \
        --test_arg=--include-ignored
    ...
    thread 'tests::test_struct_float' panicked at 'assertion failed: `(left == right)`
      left: `579.0`,
     right: `-1.5202937e29`', .../test/struct/abi_class/test.rs:15:9
    ```
    
    PiperOrigin-RevId: 511837116

---
+++ rs_bindings_from_cc/test/struct/abi_class/abi_class.h
@@ -0,0 +1,103 @@
+// Part of the Crubit project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#ifndef CRUBIT_RS_BINDINGS_FROM_CC_TEST_STRUCT_ABI_CLASS_ABI_CLASS_H_
+#define CRUBIT_RS_BINDINGS_FROM_CC_TEST_STRUCT_ABI_CLASS_ABI_CLASS_H_
+
+#pragma clang lifetime_elision
+
+// These are regression tests for b/270384825
+//
+// Notes that apply to all the structs below:
+//
+// * The structs below have different ABI classification.  We use these structs
+//   to verify if Rust bindings will use a compatible ABI.  An incompatible ABI
+//   can mean that Rust and C++ will look for a function return value in a
+//   different CPU register.  For example, https://stackoverflow.com/a/42413484
+//   points out that integers may be returned in `rax` or `rdx`, but floating
+//   point values may use `xmm0` or `xmm1`.  One risk is that the ABI
+//   classification can change when we change the type of some fields to
+//   represent them as an opaque blob of bytes - e.g.:
+//   `pub(crate) __unnamed_field0: [MaybeUninit<u8>; 4]`
+//
+// * To encourage `rs_bindings_from_cc` to represent the fields as an opaque
+//   blob of bytes (i.e. to not preserve the actual type of the field) the
+//   fields below:
+//     - are `private`
+//     - use a nested struct (an unsupported type)
+//
+// * Optimizing compiler can make the disassembly of the `Create` methods quite
+//   empty (probably because the input argument uses the same register as the
+//   return value.  To make the tests more sensitive to ABI choices, the `Add`
+//   method is used (to actually operate on the input arguments and to have to
+//   calculate a *new* return value).  For example, this is how the
+//   `StructFloat::Create` method's disassembly may look like:
+//       __rust_thunk___ZN11StructFloat6CreateEf:
+//              push   %rbp
+//              mov    %rsp,%rbp
+//              pop    %rbp
+//              ret
+
+// Expected System V ABI classification of this struct: SSE.
+// The return value is expected to be put into the `xmm0` register:
+// https://godbolt.org/z/a1aK8Yxdx
+//
+// This is a regression test for b/270454629.  Before this bug was fixed the
+// `test_struct_float` test would fail (with the actual/right value being quite
+// random - depending on what happened to be present in the `xmm0` register
+// before the test).
+struct StructFloat final {
+ public:
+  static inline StructFloat Create(float f) {
+    StructFloat s;
+    s.float_var = f;
+    return s;
+  }
+
+  static inline StructFloat Add(StructFloat x, StructFloat y) {
+    StructFloat s;
+    s.float_var = x.float_var + y.float_var;
+    return s;
+  }
+
+  static inline float Inspect(StructFloat s) { return s.float_var; }
+
+ private:
+  struct {
+    float float_var;
+  };
+};
+
+// Expected System V ABI classification of this struct: integer.
+// The return value is expected to be put into the `rax` register:
+// https://godbolt.org/z/a1aK8Yxdx
+//
+// This struct is used for tests for completeness, but it is *not* expected
+// to uncover problems caused by ABI differences, because we expect that the
+// same ABI classification (integer) will be used by the C++ struct below
+// and by the Rust struct (even if the `int_var` field would be replaced
+// by `[u8; 4]`).
+struct StructInteger final {
+ public:
+  static inline StructInteger Create(int i) {
+    StructInteger s;
+    s.int_var = i;
+    return s;
+  }
+
+  static inline StructInteger Add(StructInteger x, StructInteger y) {
+    StructInteger s;
+    s.int_var = x.int_var + y.int_var;
+    return s;
+  }
+
+  static inline int Inspect(StructInteger s) { return s.int_var; }
+
+ private:
+  struct {
+    int int_var;
+  };
+};
+
+#endif  // CRUBIT_RS_BINDINGS_FROM_CC_TEST_STRUCT_ABI_CLASS_ABI_CLASS_H_
---
+++ rs_bindings_from_cc/test/struct/abi_class/test.rs
@@ -0,0 +1,25 @@
+// Part of the Crubit project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#[cfg(test)]
+mod tests {
+    use abi_class::*;
+
+    #[ignore] // TODO(b/270454629): Re-enable the test after fixing the bug.
+    #[test]
+    fn test_struct_float() {
+        let x = StructFloat::Create(123.0);
+        let y = StructFloat::Create(456.0);
+        let sum = StructFloat::Add(x, y);
+        assert_eq!(123.0 + 456.0, StructFloat::Inspect(sum));
+    }
+
+    #[test]
+    fn test_struct_integer() {
+        let x = StructInteger::Create(456);
+        let y = StructInteger::Create(789);
+        let sum = StructInteger::Add(x, y);
+        assert_eq!(456 + 789, StructInteger::Inspect(sum));
+    }
+}
