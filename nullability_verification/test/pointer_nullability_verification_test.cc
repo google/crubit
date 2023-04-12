9fb4ed0d0f545e021d925b77534d6337759ebd54
Martin Brænne <mboehme@google.com>
2023-04-12 01:15:33 -0700

    [nullability] Split tests for temporary materialization out into temporary_materialization.cc.
    PiperOrigin-RevId: 523630667

--- nullability_verification/test/pointer_nullability_verification_test.cc
+++ nullability_verification/test/pointer_nullability_verification_test.cc
@@ -14,68 +14,6 @@ namespace tidy {
 namespace nullability {
 namespace {
 
-TEST(PointerNullabilityTest, NonFlowSensitiveMaterializeTemporaryExpr) {
-  EXPECT_TRUE(checkDiagnostics(R"cc(
-    int *_Nonnull makeNonnull();
-    int *_Nullable makeNullable();
-    int *makeUnannotated();
-
-    template <typename T>
-    T identity(const T &);
-
-    void target() {
-      {
-        *identity<int *_Nonnull>(makeNonnull());
-        int *const &p = makeNonnull();
-        *p;
-      }
-      {
-        *identity<int *_Nullable>(makeNullable());  // [[unsafe]]
-        int *const &p = makeNullable();
-        *p;  // [[unsafe]]
-      }
-      {
-        *identity<int *>(makeUnannotated());
-        int *const &p = makeUnannotated();
-        *p;
-      }
-    }
-  )cc"));
-
-  EXPECT_TRUE(checkDiagnostics(R"cc(
-    template <typename T0, typename T1>
-    struct Struct2Arg {
-      T0 getT0();
-      T1 getT1();
-    };
-
-    template <typename T>
-    T make();
-
-    template <typename T>
-    T identity(const T &);
-
-    void target(Struct2Arg<int *, int *_Nullable> &p) {
-      *identity<Struct2Arg<int *, int *_Nullable>>(p).getT0();
-      *identity<Struct2Arg<int *, int *_Nullable>>(
-           make<Struct2Arg<int *, int *_Nullable>>())
-           .getT0();
-      *identity<Struct2Arg<int *, int *_Nullable>>(
-           Struct2Arg<int *, int *_Nullable>(p))
-           .getT0();
-      *identity<int *>(p.getT0());
-      *identity<Struct2Arg<int *, int *_Nullable>>(p).getT1();  // [[unsafe]]
-      *identity<Struct2Arg<int *, int *_Nullable>>(             // [[unsafe]]
-           make<Struct2Arg<int *, int *_Nullable>>())
-           .getT1();
-      *identity<Struct2Arg<int *, int *_Nullable>>(  // [[unsafe]]
-           Struct2Arg<int *, int *_Nullable>(p))
-           .getT1();
-      *identity<int *_Nullable>(p.getT1());  // [[unsafe]]
-    }
-  )cc"));
-}
-
 TEST(PointerNullabilityTest, FunctionTemplates) {
   // Call expression that returns the first of two type parameters.
   EXPECT_TRUE(checkDiagnostics(R"cc(
