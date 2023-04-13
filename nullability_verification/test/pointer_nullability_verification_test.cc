3f01c6fc4015e82719f6c1f3652f3d5cefcca845
Martin Brænne <mboehme@google.com>
2023-04-13 03:37:39 -0700

    [nullability] Split out tests for parenthesized expressions into parens.cc.
    PiperOrigin-RevId: 523955759

--- nullability_verification/test/pointer_nullability_verification_test.cc
+++ nullability_verification/test/pointer_nullability_verification_test.cc
@@ -14,41 +14,6 @@ namespace tidy {
 namespace nullability {
 namespace {
 
-TEST(PointerNullabilityTest, ParenthesizedExpressions) {
-  EXPECT_TRUE(checkDiagnostics(R"cc(
-    template <typename T0>
-    struct Struct1Arg {
-      T0 arg0;
-      T0 getT0();
-    };
-
-    void target(Struct1Arg<int *_Nullable> p) {
-      *(p).arg0;         // [[unsafe]]
-      *((p)).arg0;       // [[unsafe]]
-      *(p).getT0();      // [[unsafe]]
-      *(((p))).getT0();  // [[unsafe]]
-    }
-  )cc"));
-
-  EXPECT_TRUE(checkDiagnostics(R"cc(
-    template <int I0, typename T1, typename T2>
-    struct Struct3ArgWithInt {
-      T1 arg1;
-      T2 arg2;
-
-      T1 getT1();
-      T2 getT2();
-    };
-
-    void target(Struct3ArgWithInt<1, int *, int *_Nullable> p) {
-      *(((p)).arg1);
-      *(((p))).getT1();
-      (*((p)).arg2);         // [[unsafe]]
-      *(((((p)))).getT2());  // [[unsafe]]
-    }
-  )cc"));
-}
-
 // TODO: fix false positives due to unsupported PointerValues in the framework.
 TEST(PointerNullabilityTest, PointerArithmetic) {
   EXPECT_TRUE(checkDiagnostics(R"cc(
