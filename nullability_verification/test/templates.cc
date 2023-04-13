c473749301aa0ecd49ed66ce8c3d093ed6b40290
Martin Brænne <mboehme@google.com>
2023-04-13 03:49:02 -0700

    [nullability] Move test `ParenTypeInTemplate` to templates.cc.
    Also, delete the now-empty pointer_nullability_verification_test.cc.
    
    PiperOrigin-RevId: 523957635

--- nullability_verification/test/templates.cc
+++ nullability_verification/test/templates.cc
@@ -1098,6 +1098,72 @@ TEST(PointerNullabilityTest, FunctionTemplates) {
   )cc"));
 }
 
+TEST(PointerNullabilityTest, ParenTypeInTemplate) {
+  checkDiagnostics(R"cc(
+    template <typename T>
+    struct S {
+      T(a);
+      T(*(b));
+
+      T (*f)();
+      T(((*g)))();
+    };
+
+    void targetNullable(S<int* _Nullable> s) {
+      *s.a;   // [[unsafe]]
+      **s.b;  // [[unsafe]]
+      *s.f;
+      *s.g;
+      // TODO: Handle function pointers. The analysis currently crashes.
+      // *s.f();
+      // *s.g();
+    }
+
+    void targetNonnull(S<int* _Nonnull> s) {
+      *s.a;
+      **s.b;
+      *s.f;
+      *s.g;
+      // TODO: Handle function pointers. The analysis currently crashes.
+      // *s.f();
+      // *s.g();
+    }
+  )cc");
+
+  checkDiagnostics(R"cc(
+    template <typename T>
+    struct S {
+      T arg;
+    };
+
+    void targetNullable(S<int* _Nullable>(a), S<int* _Nullable>(*(b)),
+                        S<int(*_Nullable)> c, S<int*(*(*_Nullable))> d,
+                        S<int* _Nullable (*)()> e) {
+      *a.arg;    // [[unsafe]]
+      *b->arg;   // [[unsafe]]
+      *c.arg;    // [[unsafe]]
+      ***d.arg;  // [[unsafe]]
+      *e.arg;    // [[unsafe]]
+
+      // TODO: Handle function pointers. The analysis currently crashes.
+      // *e.arg();
+    }
+
+    void targetNonnull(S<int* _Nonnull>(a), S<int* _Nonnull>(*(b)),
+                       S<int(*_Nonnull)> c, S<int*(*(*_Nonnull))> d,
+                       S<int* _Nonnull (*)()> e) {
+      *a.arg;
+      *b->arg;
+      *c.arg;
+      ***d.arg;
+      *e.arg;
+
+      // TODO: Handle function pointers. The analysis currently crashes.
+      // *e.arg();
+    }
+  )cc");
+}
+
 }  // namespace
 }  // namespace nullability
 }  // namespace tidy
