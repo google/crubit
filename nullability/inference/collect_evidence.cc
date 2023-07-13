e9210aa39fe86aed03ab5aac8d9aa9f6e9626c77
Googler <no-reply@google.com>
2023-07-13 10:55:06 -0700

    Collect nullability Evidence for function parameters from dereferences.
    Revive and update tests for dereference detection.
    
    Also remove some unneeded clang:: namespacing.
    
    PiperOrigin-RevId: 547851980
    Change-Id: I6705baa32ed0646633a98a13e984b57dd43de9c2

--- nullability/inference/collect_evidence.cc
+++ nullability/inference/collect_evidence.cc
@@ -5,15 +5,20 @@
 #include "nullability/inference/collect_evidence.h"
 
 #include <optional>
+#include <string>
+#include <utility>
 #include <vector>
 
 #include "absl/log/check.h"
 #include "nullability/inference/inference.proto.h"
+#include "nullability/pointer_nullability.h"
 #include "nullability/pointer_nullability_analysis.h"
 #include "nullability/pointer_nullability_lattice.h"
 #include "clang/AST/ASTContext.h"
 #include "clang/AST/Decl.h"
 #include "clang/AST/DeclBase.h"
+#include "clang/AST/Expr.h"
+#include "clang/AST/OperationKinds.h"
 #include "clang/AST/Stmt.h"
 #include "clang/AST/Type.h"
 #include "clang/Analysis/CFG.h"
@@ -21,6 +26,7 @@
 #include "clang/Analysis/FlowSensitive/DataflowAnalysis.h"
 #include "clang/Analysis/FlowSensitive/DataflowAnalysisContext.h"
 #include "clang/Analysis/FlowSensitive/DataflowEnvironment.h"
+#include "clang/Analysis/FlowSensitive/Value.h"
 #include "clang/Analysis/FlowSensitive/WatchedLiteralsSolver.h"
 #include "clang/Basic/LLVM.h"
 #include "clang/Index/USRGeneration.h"
@@ -32,36 +38,115 @@ namespace clang::tidy::nullability {
 using ::clang::dataflow::DataflowAnalysisContext;
 using ::clang::dataflow::Environment;
 
-llvm::Expected<std::vector<Evidence>> collectEvidence(
-    const clang::FunctionDecl &Func, clang::ASTContext &Context) {
+namespace {
+std::optional<Evidence> collectEvidenceFromDereference(
+    const FunctionDecl &Func,
+    std::vector<std::pair<PointerTypeNullability, Slot>> InferrableSlots,
+    const std::string &USR, const CFGElement &Element,
+    const PointerNullabilityLattice &Lattice,
+    const dataflow::Environment &Env) {
+  // Is this CFGElement a dereference of a pointer?
+  auto CFGStmt = Element.getAs<clang::CFGStmt>();
+  if (!CFGStmt) return std::nullopt;
+  auto *Op = dyn_cast_or_null<UnaryOperator>(CFGStmt->getStmt());
+  if (!Op || Op->getOpcode() != UO_Deref) return std::nullopt;
+  auto *DereferencedExpr = Op->getSubExpr();
+  if (!DereferencedExpr || !DereferencedExpr->getType()->isPointerType())
+    return std::nullopt;
+
+  // It is a dereference of a pointer. Now gather evidence from it.
+  dataflow::PointerValue *DereferencedValue =
+      getPointerValueFromExpr(DereferencedExpr, Env);
+  if (!DereferencedValue) return std::nullopt;
+  auto &A = Env.getDataflowAnalysisContext().arena();
+  auto &NotIsNull =
+      A.makeNot(getPointerNullState(*DereferencedValue).second.formula());
+
+  // If the flow conditions already imply the dereferenced value is not null,
+  // then we don't have any new evidence of a necessary annotation.
+  if (Env.flowConditionImplies(NotIsNull)) return std::nullopt;
+
+  // Otherwise, if an inferrable slot being annotated Nonnull would imply that
+  // the dereferenced value is not null, then we have evidence suggesting that
+  // slot should be annotated. For now, we simply choose the first such slot,
+  // sidestepping complexities around the possibility of multiple such slots,
+  // any one of which would be sufficient if annotated Nonnull.
+  for (auto &[Nullability, Slot] : InferrableSlots) {
+    auto &SlotNonnullImpliesDerefValueNonnull =
+        A.makeImplies(Nullability.Nonnull->formula(), NotIsNull);
+    if (Env.flowConditionImplies(SlotNonnullImpliesDerefValueNonnull)) {
+      Evidence Evidence;
+      Evidence.mutable_constraint()->set_must_be_nonnull(true);
+      *Evidence.mutable_slot() = Slot;
+      Evidence.mutable_symbol()->set_usr(USR);
+      return Evidence;
+    }
+  }
+
+  return std::nullopt;
+}
+
+void appendEvidence(
+    std::vector<Evidence> &AllEvidence, const FunctionDecl &Func,
+    std::vector<std::pair<PointerTypeNullability, Slot>> InferrableSlots,
+    const std::string &USR, const CFGElement &Element,
+    const PointerNullabilityLattice &Lattice, const Environment &Env) {
+  if (std::optional<Evidence> NewEvidence = collectEvidenceFromDereference(
+          Func, InferrableSlots, USR, Element, Lattice, Env)) {
+    AllEvidence.push_back(std::move(*NewEvidence));
+  }
+  // TODO: add more heuristic collections here
+}
+}  // namespace
+
+llvm::Expected<std::vector<Evidence>> collectEvidence(const FunctionDecl &Func,
+                                                      ASTContext &Context) {
   // We want to make sure we use the declaration that the body comes from,
   // otherwise we will see references to `ParmVarDecl`s from a different
   // declaration.
-  const clang::FunctionDecl *DeclWithBody = nullptr;
+  const FunctionDecl *DeclWithBody = nullptr;
   if (!Func.getBody(DeclWithBody)) {
     return llvm::make_error<llvm::StringError>(llvm::errc::invalid_argument,
                                                "Function has no body.");
   }
   CHECK(DeclWithBody);
 
-  llvm::Expected<clang::dataflow::ControlFlowContext> ControlFlowContext =
-      clang::dataflow::ControlFlowContext::build(*DeclWithBody);
+  llvm::Expected<dataflow::ControlFlowContext> ControlFlowContext =
+      dataflow::ControlFlowContext::build(*DeclWithBody);
   if (!ControlFlowContext) return ControlFlowContext.takeError();
 
   DataflowAnalysisContext AnalysisContext(
-      std::make_unique<clang::dataflow::WatchedLiteralsSolver>());
+      std::make_unique<dataflow::WatchedLiteralsSolver>());
   Environment Environment(AnalysisContext, *DeclWithBody);
   PointerNullabilityAnalysis Analysis(Context);
+  std::vector<std::pair<PointerTypeNullability, Slot>> InferrableSlots;
+  auto Parameters = Func.parameters();
+  for (auto i = 0; i < Parameters.size(); ++i) {
+    if (Parameters[i]->getType().getNonReferenceType()->isPointerType()) {
+      // TODO: Skip assigning variables for already-annotated parameters,
+      // potentially configurably.
+      Slot slot;
+      slot.set_parameter(i);
+      InferrableSlots.push_back(
+          std::make_pair(Analysis.assignNullabilityVariable(
+                             Parameters[i], AnalysisContext.arena()),
+                         std::move(slot)));
+    }
+  }
 
   std::vector<Evidence> AllEvidence;
+  llvm::SmallString<128> USR;
+  index::generateUSRForDecl(&Func, USR);
   llvm::Expected<std::vector<std::optional<
-      clang::dataflow::DataflowAnalysisState<PointerNullabilityLattice>>>>
-      BlockToOutputStateOrError = clang::dataflow::runDataflowAnalysis(
+      dataflow::DataflowAnalysisState<PointerNullabilityLattice>>>>
+      BlockToOutputStateOrError = dataflow::runDataflowAnalysis(
           *ControlFlowContext, Analysis, Environment,
-          [](const CFGElement &Element,
-             const dataflow::DataflowAnalysisState<PointerNullabilityLattice>
-                 &State) {
-            // TODO: collect Evidence values from element
+          [&](const CFGElement &Element,
+              const dataflow::DataflowAnalysisState<PointerNullabilityLattice>
+                  &State) {
+            appendEvidence(AllEvidence, *DeclWithBody, InferrableSlots,
+                           std::string(USR.data(), USR.size()), Element,
+                           State.Lattice, State.Env);
           });
 
   return AllEvidence;
--- nullability/inference/collect_evidence_test.cc
+++ nullability/inference/collect_evidence_test.cc
@@ -14,12 +14,14 @@
 #include "third_party/llvm/llvm-project/clang/unittests/Analysis/FlowSensitive/TestingSupport.h"
 #include "llvm/ADT/StringRef.h"
 #include "llvm/Support/Error.h"
-#include "third_party/llvm/llvm-project/third-party/unittest/googlemock/include/gmock/gmock.h"
+#include "third_party/llvm/llvm-project/third-party/unittest/googlemock/include/gmock/gmock.h"  // IWYU pragma: keep
 #include "third_party/llvm/llvm-project/third-party/unittest/googletest/include/gtest/gtest.h"
 
 namespace clang::tidy::nullability {
 namespace {
+using ::testing::HasSubstr;
 using ::testing::IsEmpty;
+using ::testing::UnorderedElementsAre;
 
 std::vector<Evidence> collectEvidenceFromTargetFunction(
     llvm::StringRef Source) {
@@ -30,6 +32,13 @@ std::vector<Evidence> collectEvidenceFromTargetFunction(
                       AST.context());
   CHECK(Results) << toString(Results.takeError());
   return *Results;
+}
+
+MATCHER_P(isNonnullEvidenceForTargetParamAtIndex, ParamIndex, "") {
+  return ExplainMatchResult(HasSubstr("target"), arg.symbol().usr(),
+                            result_listener) &&
+         arg.slot().parameter() == ParamIndex &&
+         arg.constraint().must_be_nonnull();
 }
 
 TEST(InferAnnotationsTest, NoParams) {
@@ -53,6 +62,137 @@ TEST(InferAnnotationsTest, OneParamUsedWithoutRestriction) {
     void target(int *p0) { takesUnknown(p0); }
   )cc";
   EXPECT_THAT(collectEvidenceFromTargetFunction(Src), IsEmpty());
+}
+
+TEST(InferAnnotationsTest, Deref) {
+  static constexpr llvm::StringRef Src = R"cc(
+    void target(int *p0, int *p1) {
+      int a = *p0;
+      if (p1 != nullptr) {
+        int b = *p1;
+      }
+    }
+  )cc";
+  EXPECT_THAT(collectEvidenceFromTargetFunction(Src),
+              UnorderedElementsAre(isNonnullEvidenceForTargetParamAtIndex(0)));
+}
+
+TEST(InferAnnotationsTest, DereferenceBeforeAssignment) {
+  static constexpr llvm::StringRef Src = R"cc(
+    void target(int *p) {
+      *p;
+      int i = 1;
+      p = &i;
+    }
+  )cc";
+  EXPECT_THAT(collectEvidenceFromTargetFunction(Src),
+              UnorderedElementsAre(isNonnullEvidenceForTargetParamAtIndex(0)));
+}
+
+TEST(InferAnnotationsTest, DereferenceAfterAssignment) {
+  static constexpr llvm::StringRef Src = R"cc(
+    void target(int *p) {
+      int i = 1;
+      p = &i;
+      *p;
+    }
+  )cc";
+  EXPECT_THAT(collectEvidenceFromTargetFunction(Src), IsEmpty());
+}
+
+TEST(InferAnnotationsTest, DerefOfPtrRef) {
+  static constexpr llvm::StringRef Src = R"cc(
+    void target(int *&p0, int *&p1) {
+      int a = *p0;
+      if (p1 != nullptr) {
+        int b = *p1;
+      }
+    }
+  )cc";
+  EXPECT_THAT(collectEvidenceFromTargetFunction(Src),
+              UnorderedElementsAre(isNonnullEvidenceForTargetParamAtIndex(0)));
+}
+
+TEST(InferAnnotationsTest, UnrelatedCondition) {
+  static constexpr llvm::StringRef Src = R"cc(
+    void target(int *p0, int *p1, int *p2, bool b) {
+      if (b) {
+        int a = *p0;
+        int b = *p1;
+      } else {
+        int a = *p0;
+        int c = *p2;
+      }
+    }
+  )cc";
+  EXPECT_THAT(collectEvidenceFromTargetFunction(Src),
+              UnorderedElementsAre(
+                  isNonnullEvidenceForTargetParamAtIndex(0),
+                  isNonnullEvidenceForTargetParamAtIndex(1),
+                  // We collect two Evidence values for two dereferences of p0
+                  isNonnullEvidenceForTargetParamAtIndex(0),
+                  isNonnullEvidenceForTargetParamAtIndex(2)));
+}
+
+TEST(InferAnnotationsTest, LaterDeref) {
+  static constexpr llvm::StringRef Src = R"cc(
+    void target(int *p0) {
+      if (p0 == nullptr) {
+        (void)0;
+      } else {
+        (void)0;
+      }
+      int a = *p0;
+    }
+  )cc";
+  EXPECT_THAT(collectEvidenceFromTargetFunction(Src),
+              UnorderedElementsAre(isNonnullEvidenceForTargetParamAtIndex(0)));
+}
+
+TEST(InferAnnotationsTest, DerefBeforeGuardedDeref) {
+  static constexpr llvm::StringRef Src = R"cc(
+    void target(int *p0) {
+      int a = *p0;
+      if (p0 != nullptr) {
+        int b = *p0;
+      }
+    }
+  )cc";
+  EXPECT_THAT(collectEvidenceFromTargetFunction(Src),
+              UnorderedElementsAre(isNonnullEvidenceForTargetParamAtIndex(0)));
+}
+
+TEST(InferAnnotationsTest, EarlyReturn) {
+  static constexpr llvm::StringRef Src = R"cc(
+    void target(int *p0) {
+      if (!p0) {
+        return;
+      }
+      int a = *p0;
+    }
+  )cc";
+  EXPECT_THAT(collectEvidenceFromTargetFunction(Src), IsEmpty());
+}
+
+TEST(InferAnnotationsTest, UnreachableCode) {
+  static constexpr llvm::StringRef Src = R"cc(
+    void target(int *p0, int *p1, int *p2, int *p3) {
+      if (true) {
+        int a = *p0;
+      } else {
+        int a = *p1;
+      }
+
+      if (false) {
+        int a = *p2;
+      }
+
+      return;
+      int a = *p3;
+    }
+  )cc";
+  EXPECT_THAT(collectEvidenceFromTargetFunction(Src),
+              UnorderedElementsAre(isNonnullEvidenceForTargetParamAtIndex(0)));
 }
 
 }  // namespace
