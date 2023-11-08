34e070dd979bc7b9c1130c6dabf15df5a8f90864
Jing Lu <lujin@google.com>
2023-11-08 02:47:10 -0800

    rs_bindings_from_cc: Add a reproducer that a forward declaration in a different target precludes bindings of actual definition.
    PiperOrigin-RevId: 580463296
    Change-Id: Id9ff93b5a8ca8b8d2407af18de0d7e1a9cbcc59b

---
+++ rs_bindings_from_cc/test/forward_declaration/test.rs
@@ -0,0 +1,20 @@
+// Part of the Crubit project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#[cfg(test)]
+mod tests {
+    use item_exists::type_exists;
+
+    #[test]
+    fn test_same_library() {
+        assert!(type_exists!(definition_and_forward_declaration_same_cc_library::A));
+    }
+
+    // TODO(b/309614052): Currently Crubit cannot generate bindings if the forward
+    // declaration and definition are from different targets.
+    #[test]
+    fn test_separate_library() {
+        assert!(!type_exists!(definition_and_forward_declaration_separate_cc_library::A));
+    }
+}
