34063abce1bb24db40959b4b08b77583f24aa509
Jing Lu <lujin@google.com>
2023-11-28 02:44:42 -0800

    rs_bindings_from_cc: Add reproducers for a C++ target with invalid character (for Rust crate name).
    With 3 different cases:
    
    A. With no Crubit in the picture. Useful for separating the issue between the Rust build rule and Crubit bazel integration.
    
    B. Crubit enabled for the cc_library (with valid character) + Crubit-disabled for the cc_library with invalid character. Useful for assessing how much the invalid character is going to impact Crubit (direct) clients.
    
    C. Crubit enabled for the cc_library with invalid character. Useful for understanding how Crubit (in the future) maps the invalid character.
    
    PiperOrigin-RevId: 585905841
    Change-Id: I82569db40d34e5b56517093346d0e5076925e8d2

---
+++ rs_bindings_from_cc/test/cpp_target_name/header.h
@@ -0,0 +1,9 @@
+// Part of the Crubit project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#ifndef THIRD_PARTY_CRUBIT_RS_BINDINGS_FROM_CC_TEST_CPP_TARGET_NAME_HEADER_H_
+#define THIRD_PARTY_CRUBIT_RS_BINDINGS_FROM_CC_TEST_CPP_TARGET_NAME_HEADER_H_
+
+void inline func() {}
+
+#endif  // THIRD_PARTY_CRUBIT_RS_BINDINGS_FROM_CC_TEST_CPP_TARGET_NAME_HEADER_H_
---
+++ rs_bindings_from_cc/test/cpp_target_name/test_with_crubit_enabled_cc_library_with_invalid_character.rs
@@ -0,0 +1,13 @@
+// Part of the Crubit project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#[cfg(test)]
+
+mod tests {
+    use crubit_enabled_cc_library_with__________________________::func;
+
+    #[test]
+    fn test_build_with_crubit_enabled_cc_library_with_invalid_character() {
+        func();
+    }
+}
