d83a8a02114a4f1353887e5a85814e8bcc9d8029
Jing Lu <lujin@google.com>
2023-11-30 09:54:58 -0800

    rs_bindings_from_cc: Remove test/escaping_target_names/.
    It's superceded by test/cpp_target_name, which covers more invalid characters and scenarios on whether or not Crubit is enabled.
    
    PiperOrigin-RevId: 586705620
    Change-Id: Ie7627946876c9ee13ebeef788d7e0c29bf2e8ecb

--- rs_bindings_from_cc/test/escaping_target_names/uses-target-with-dashes.cc
+++
@@ -1,9 +0,0 @@
-// Part of the Crubit project, under the Apache License v2.0 with LLVM
-// Exceptions. See /LICENSE for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-
-#include "rs_bindings_from_cc/test/escaping_target_names/uses-target-with-dashes.h"
-
-#include "rs_bindings_from_cc/test/escaping_target_names/has-dashes-in-name.h"
-
-int SomeFunc(SomeStruct s) { return s.value; }
