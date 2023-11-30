104809862b6ff92d5c6d11eba158cb943d84c951
Jing Lu <lujin@google.com>
2023-11-30 10:48:32 -0800

    rs_bindings_from_cc: Add reproducer that Crubit crashes when encountering a function that references a forward-declared type by value as parameter or return type.
    PiperOrigin-RevId: 586725043
    Change-Id: I1362ee0e0d0fa644d7e5d89aaa92b8a1f77229e9

---
+++ rs_bindings_from_cc/test/struct/incomplete_record/test.rs
@@ -0,0 +1,11 @@
+// Part of the Crubit project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#[cfg(test)]
+
+mod tests {
+
+    #[test]
+    fn test_build() {}
+}
