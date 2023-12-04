95df6a79eff87da698c31c3a17b9b6acef70ad90
Martin Brænne <mboehme@google.com>
2023-12-04 06:58:25 -0800

    [nullability] Add the shell of a `nullability_test` for smart pointers.
    At the moment, the only this checks is that we can include <memory>.
    
    PiperOrigin-RevId: 587705194
    Change-Id: I9ae4e23b2d96cca1c9011fad67180c50167488b1

---
+++ nullability/test/smart_pointers.cc
@@ -0,0 +1,9 @@
+// Part of the Crubit project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+// Tests for smart pointers.
+
+#include <memory>
+
+#include "nullability_test.h"
