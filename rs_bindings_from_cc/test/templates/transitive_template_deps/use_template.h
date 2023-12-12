2643cdf675baa9baca06c68ff718359b299c7a9a
Jing Lu <lujin@google.com>
2023-12-12 05:47:33 -0800

    Add reproducer that `std::bitset<1>` causes Crubit to crash:
    Couldn't find decl_id ItemId(...) in the IR.
    
    PiperOrigin-RevId: 590172166
    Change-Id: I00c2a1565b563c8dbf4ad1b4cc94f43e1fedc33f

---
+++ rs_bindings_from_cc/test/templates/transitive_template_deps/use_template.h
@@ -0,0 +1,12 @@
+// Part of the Crubit project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#ifndef THIRD_PARTY_CRUBIT_RS_BINDINGS_FROM_CC_TEST_TEMPLATES_TRANSITIVE_TEMPLATE_DEPS_USE_TEMPLATE_H_
+#define THIRD_PARTY_CRUBIT_RS_BINDINGS_FROM_CC_TEST_TEMPLATES_TRANSITIVE_TEMPLATE_DEPS_USE_TEMPLATE_H_
+
+#include <bitset>
+
+inline void Func(std::bitset<1>){};
+
+#endif  // THIRD_PARTY_CRUBIT_RS_BINDINGS_FROM_CC_TEST_TEMPLATES_TRANSITIVE_TEMPLATE_DEPS_USE_TEMPLATE_H_
