b62c0e00061b25c7673a3544ff511fb30bd1ae86
Jing Lu <lujin@google.com>
2023-12-13 01:31:59 -0800

    rs_bindings_from_cc: Add reproducer that `copts` from transitive dependencies are dropped.
    PiperOrigin-RevId: 590498785
    Change-Id: I33463049cd851140f24c1dc5eb080de628f3edf1

---
+++ rs_bindings_from_cc/test/transitive_deps_copt/use_dep.h
@@ -0,0 +1,12 @@
+// Part of the Crubit project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#ifndef THIRD_PARTY_CRUBIT_RS_BINDINGS_FROM_CC_TEST_TRANSITIVE_DEPS_COPT_USE_DEP_H_
+#define THIRD_PARTY_CRUBIT_RS_BINDINGS_FROM_CC_TEST_TRANSITIVE_DEPS_COPT_USE_DEP_H_
+
+#include "rs_bindings_from_cc/test/transitive_deps_copt/transitive_dep.h"
+
+inline void Func() { FuncA(); }
+
+#endif  // THIRD_PARTY_CRUBIT_RS_BINDINGS_FROM_CC_TEST_TRANSITIVE_DEPS_COPT_USE_DEP_H_
