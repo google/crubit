c8c5185ca5a615b3ebc8dad08b83f15a9f87b1fd
Jing Lu <lujin@google.com>
2024-01-15 02:20:47 -0800

    rs_bindings_from_cc: Add test cases for using forward declarations across crate boundaries.
    PiperOrigin-RevId: 598552030
    Change-Id: Iacd0272d96772bc85187318851ab1d21b4f745d9

---
+++ rs_bindings_from_cc/test/forward_declaration/multiple_forward_declarations_dependent/test.rs
@@ -0,0 +1,46 @@
+// Part of the Crubit project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+use core::any::TypeId;
+use forward_declare::CcCast;
+use item_exists::type_exists;
+
+#[test]
+fn test_complete_to_incomplete_ptr_conversion_crossing_crate_boundaries() {
+    let mut a = definition::A::default();
+    let a_ptr: *mut definition::A = &mut a;
+    unsafe {
+        let identity1 = forward_declaration1::IdentityPtr1(a_ptr.cc_cast());
+        let identity2 = forward_declaration2::IdentityPtr2(a_ptr.cc_cast());
+
+        let identity2_from_identity1 = forward_declaration2::IdentityPtr2(identity1.cc_cast());
+        let identity1_from_identity2 = forward_declaration1::IdentityPtr1(identity2.cc_cast());
+
+        let _ = forward_declaration1::IdentityPtr1(identity2_from_identity1.cc_cast());
+        let _ = forward_declaration2::IdentityPtr2(identity1_from_identity2.cc_cast());
+    }
+}
+
+#[test]
+fn test_complete_to_incomplete_ref_conversion_crossing_crate_boundaries() {
+    let mut a = definition::A::default();
+    let a_ptr: *mut definition::A = &mut a;
+    unsafe {
+        let identity1 = forward_declaration1::IdentityPtr1(a_ptr.cc_cast());
+        let identity2 = forward_declaration2::IdentityPtr2(a_ptr.cc_cast());
+
+        let identity2_from_identity1 = forward_declaration2::IdentityPtr2(identity1.cc_cast());
+        let identity1_from_identity2 = forward_declaration1::IdentityPtr1(identity2.cc_cast());
+
+        let _ = forward_declaration1::IdentityPtr1(identity2_from_identity1.cc_cast());
+        let _ = forward_declaration2::IdentityPtr2(identity1_from_identity2.cc_cast());
+    }
+}
+
+#[test]
+fn test_each_crate_has_distinct_type_for_a() {
+    assert_ne!(TypeId::of::<forward_declaration1::A>(), TypeId::of::<definition::A>());
+    // Note: forward_declaration2 re-uses the (incomplete) type in
+    // forward_declaration1 for A.
+    assert!(!type_exists!(forward_declaration2::A));
+}
