4f63e890bbd784c0cdc4b8abbed358350d99ae3b
Krasimir Georgiev <krasimir@google.com>
2024-03-14 08:13:18 -0700

    crubit: adapt for https://github.com/rust-lang/rust/commit/63091b105d08b7b0db19d699d3be3060acde04ad
    PiperOrigin-RevId: 615783604
    Change-Id: I1432904e41daf72d202b121cf95aef95706890e2

--- cc_bindings_from_rs/cmdline.rs
+++ cc_bindings_from_rs/cmdline.rs
@@ -77,7 +77,10 @@ impl Cmdline {
         //
         // TODO(b/254688847): Decide whether to replace this with a `clap`-declared,
         // `--help`-exposed `--flagfile <path>`.
-        let args = rustc_driver::args::arg_expand_all(&early_error_handler, args);
+        let args = match rustc_driver::args::arg_expand_all(&early_error_handler, args) {
+            Ok(args) => args,
+            Err(_) => bail!("Error from rustc_driver::args::arg_expand_all"),
+        };
 
         // Parse `args` using the parser `derive`d by the `clap` crate.
         let mut cmdline = Self::try_parse_from(args)?;
