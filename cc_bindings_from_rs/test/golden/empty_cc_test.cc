211b1e9b69bbd1fe1ba7eb89be8527bac739d054
Yongheng Chen <yongheng@google.com>
2024-08-21 10:29:00 -0700

    Check whether tests in cc_bindings_from_rust golden is compilable.
    PiperOrigin-RevId: 665935855
    Change-Id: Id823bfd61e802ac755b9eeea8b4a0769dd13f74d

---
+++ cc_bindings_from_rs/test/golden/empty_cc_test.cc
@@ -0,0 +1,8 @@
+// Part of the Crubit project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+// The purpose of this `cc_test` is to trigger compilation of the bindings
+// this target depends on.
+
+int main() { return 0; }
