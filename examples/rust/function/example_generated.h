8888b40fcc89ee35bc00cdc45ed3f9a04e558e52
Devin Jeanpierre <jeanpierreda@google.com>
2024-09-30 16:31:08 -0700

    Add generated golden for the `function` example.
    PiperOrigin-RevId: 680761134
    Change-Id: I2c2d144949e165a71fd5d59be0377101c1eee0f5

---
+++ examples/rust/function/example_generated.h
@@ -0,0 +1,31 @@
+// Part of the Crubit project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+// Automatically @generated C++ bindings for the following Rust crate:
+// example_crate
+// Features: experimental, supported
+
+// clang-format off
+#pragma once
+
+#include <cstdint>
+
+namespace example_crate {
+
+// Generated from:
+// examples/rust/function/example.rs;l=5
+std::int32_t add_two_integers(std::int32_t x, std::int32_t y);
+
+namespace __crubit_internal {
+extern "C" std::int32_t
+__crubit_thunk__uRNvCsh2mUQSogkZl_u13example_ucrate16add_utwo_uintegers(
+    std::int32_t, std::int32_t);
+}
+inline std::int32_t add_two_integers(std::int32_t x, std::int32_t y) {
+  return __crubit_internal::
+      __crubit_thunk__uRNvCsh2mUQSogkZl_u13example_ucrate16add_utwo_uintegers(
+          x, y);
+}
+
+}  // namespace example_crate
