b121791be50ea124947900d215debae366d16533
Googler <no-reply@google.com>
2024-10-21 01:03:25 -0700

    Use static_assert instead of EXPECT_TRUE for compile-time known values in primitive_types_test.cc
    Let's shift left in discovering compile-time-known issues, a static_assert fires earlier and consumes less computational resources to do so.
    
    PiperOrigin-RevId: 688029725
    Change-Id: I898d8d2ca5c842e0d6d21b00b09828e72501893c

--- cc_bindings_from_rs/test/primitive_types/primitive_types_test.cc
+++ cc_bindings_from_rs/test/primitive_types/primitive_types_test.cc
@@ -21,27 +21,25 @@ TEST(PrimitiveTypesTest, CVoidPtr) {
           &string, &string);
   EXPECT_EQ(s.ptr_mut, &string);
   EXPECT_EQ(s.ptr_const, &string);
-  // We need an extra set of parentheses to avoid "too many arguments provided
-  // to function-like macro invocation" when `EXPECT_TRUE` is used together with
-  // `std::is_same_v<T, U>`.
-  EXPECT_TRUE((std::is_same_v<void*, decltype(s.ptr_mut)>));
-  EXPECT_TRUE((std::is_same_v<const void*, decltype(s.ptr_const)>));
+  static_assert(std::is_same_v<void*, decltype(s.ptr_mut)>);
+  static_assert(std::is_same_v<const void*, decltype(s.ptr_const)>);
 
   const void* string_const_ptr =
       primitive_types::test_c_void_ptr::identity_const_c_void_ptr(&string);
   EXPECT_EQ(string_const_ptr, &string);
-  EXPECT_TRUE((std::is_same_v<const void*, decltype(string_const_ptr)>));
-  EXPECT_TRUE((std::is_same_v<const void*(const void*),
-                              decltype(primitive_types::test_c_void_ptr::
-                                           identity_const_c_void_ptr)>));
+  static_assert(std::is_same_v<const void*, decltype(string_const_ptr)>);
+  static_assert(std::is_same_v<const void*(const void*),
+                               decltype(primitive_types::test_c_void_ptr::
+                                            identity_const_c_void_ptr)>);
 
   void* string_mut_ptr =
       primitive_types::test_c_void_ptr::identity_mut_c_void_ptr(&string);
   EXPECT_EQ(string_mut_ptr, &string);
-  EXPECT_TRUE((std::is_same_v<void*, decltype(string_mut_ptr)>));
-  EXPECT_TRUE(
-      (std::is_same_v<void*(void*), decltype(primitive_types::test_c_void_ptr::
-                                                 identity_mut_c_void_ptr)>));
+  static_assert(std::is_same_v<void*, decltype(string_mut_ptr)>);
+  static_assert(
+      std::is_same_v<
+          void*(void*),
+          decltype(primitive_types::test_c_void_ptr::identity_mut_c_void_ptr)>);
 }
 
 TEST(PrimitiveTypesTest, MaybeUninitTest) {
