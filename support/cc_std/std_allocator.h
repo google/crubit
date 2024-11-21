0216210d96edbc6978b6d3f53063c966f9d800cb
Yongheng Chen <yongheng@google.com>
2024-11-21 15:51:05 -0800

    Automatic code cleanup.
    PiperOrigin-RevId: 698942670
    Change-Id: I76e0eb70c9c0e58fc1419cccecc4c57678475171

--- support/cc_std/std_allocator.h
+++ support/cc_std/std_allocator.h
@@ -10,28 +10,6 @@
 #include <cstddef>
 #include <new>
 
-// TODO: Remove this namespace.
-namespace crubit_internal {
-
-// TODO: Refactor to a constant when Crubit supports them.
-enum StdCppDefaultNewAlignment : size_t {
-  Value = __STDCPP_DEFAULT_NEW_ALIGNMENT__,
-};
-
-inline void* cpp_new(size_t n) { return operator new(n); }
-
-inline void* cpp_new_with_alignment(size_t n, size_t align) {
-  return operator new(n, static_cast<std::align_val_t>(align));
-}
-
-inline void cpp_delete(void* ptr, size_t n) { operator delete(ptr); }
-
-inline void cpp_delete_with_alignment(void* ptr, size_t n, size_t align) {
-  operator delete(ptr, static_cast<std::align_val_t>(align));
-}
-
-}  // namespace crubit_internal
-
 namespace crubit_cc_std_internal::std_allocator {
 
 // Copy from above. `using ns::Function` is not supported yet by Crubit. So
