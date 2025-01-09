b39f325fb693e0a3b37579a350bef637d3e5de0d
Martin Brænne <mboehme@google.com>
2025-01-09 00:50:50 -0800

    Add annotation_reader_test.cc.
    This adds some basic unit tests for `GetAnnotateAttr()`. I plan to add more unit
    tests here in a follow up patch that changes `GetAnnotateAttr()` to disallow
    conflicting annotations.
    
    PiperOrigin-RevId: 713572107
    Change-Id: I9125463af2cb82d9728b951f1208404828645dfa

---
+++ common/annotation_reader_test.cc
@@ -0,0 +1,55 @@
+// Part of the Crubit project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include "common/annotation_reader.h"
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "absl/strings/string_view.h"
+#include "common/status_test_matchers.h"
+#include "clang/Testing/TestAST.h"
+
+namespace crubit {
+namespace {
+
+using testing::HasSubstr;
+using testing::IsNull;
+using testing::NotNull;
+
+template <class T>
+T& LookupDecl(clang::ASTContext& context, absl::string_view name) {
+  clang::DeclContextLookupResult result =
+      context.getTranslationUnitDecl()->lookup(&context.Idents.get(name));
+  CHECK(result.isSingleResult());
+  return *cast<T>(result.front());
+}
+
+TEST(AnnotationReaderTest, GetAnnotateAttrSuccess) {
+  clang::TestAST ast(R"cc(
+    [[clang::annotate("foo")]] int i;
+  )cc");
+
+  auto& var = LookupDecl<clang::VarDecl>(ast.context(), "i");
+
+  EXPECT_THAT(GetAnnotateAttr(var, "foo"), IsOkAndHolds(NotNull()));
+  EXPECT_THAT(GetAnnotateAttr(var, "bar"), IsOkAndHolds(IsNull()));
+}
+
+TEST(AnnotationReaderTest, GetAnnotateAttrFailureDoubleAnnotation) {
+  clang::TestAST ast(R"cc(
+    [[clang::annotate("foo")]] [[clang::annotate("foo")]] int i;
+  )cc");
+
+  auto& var = LookupDecl<clang::VarDecl>(ast.context(), "i");
+
+  EXPECT_THAT(
+      GetAnnotateAttr(var, "foo"),
+      StatusIs(
+          absl::StatusCode::kInvalidArgument,
+          HasSubstr(
+              "Only one `foo` attribute may be placed on a declaration.")));
+}
+
+}  // namespace
+}  // namespace crubit
