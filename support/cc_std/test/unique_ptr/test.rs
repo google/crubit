de7943e09fc01c181c04cead4c046804837b8568
Devin Jeanpierre <jeanpierreda@google.com>
2025-04-14 16:09:28 -0700

    Fix UB in unique_ptr's own tests!
    unique_ptr notes that the pointer must be allocated by `new`, but in tests, actually uses a size+align `operator new` call which is flagged by ASan.
    
    This wasn't caught during testing because our AddressSanitizer tests were turned off. :(
    
    PiperOrigin-RevId: 747605852
    Change-Id: Ic7e3c0609dfd15a9ad7398c889bfcd4f642745fa

--- support/cc_std/test/unique_ptr/test.rs
+++ support/cc_std/test/unique_ptr/test.rs
@@ -3,68 +3,85 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 use googletest::prelude::*;
-use std::cell::RefCell;
-use std::rc::Rc;
+use std::sync::atomic::{AtomicI32, Ordering};
 
-struct InstanceCounted {
-    counter: Rc<RefCell<i32>>,
-}
+static INSTANCE_COUNTER: AtomicI32 = AtomicI32::new(0);
+
+/// A struct with the same size and alignment as `int`.
+/// The number of alive instances is tracked by `INSTANCE_COUNTER`.
+#[repr(transparent)]
+struct InstanceCounted(std::ffi::c_int);
 
 impl Drop for InstanceCounted {
     fn drop(&mut self) {
-        *self.counter.borrow_mut() -= 1;
+        INSTANCE_COUNTER.fetch_sub(1, Ordering::Release);
     }
 }
 
 impl InstanceCounted {
-    fn new(counter: Rc<RefCell<i32>>) -> InstanceCounted {
-        *counter.borrow_mut() += 1;
-        InstanceCounted { counter }
+    fn new() -> InstanceCounted {
+        INSTANCE_COUNTER.fetch_add(1, Ordering::Acquire);
+        InstanceCounted(123456)
     }
 
-    fn new_from_cpp_allocator(counter: Rc<RefCell<i32>>) -> *mut InstanceCounted {
-        let buffer = cc_std::crubit_cc_std_internal::std_allocator::cpp_new_with_alignment(
-            core::mem::size_of::<InstanceCounted>(),
-            core::mem::align_of::<InstanceCounted>(),
-        ) as *mut InstanceCounted;
+    fn new_unique_ptr() -> cc_std::std::unique_ptr<InstanceCounted> {
+        let mut int_sized_unique_ptr = test_helpers::unique_ptr_test::create_unique_ptr();
+        let p = int_sized_unique_ptr.release() as *mut InstanceCounted;
         unsafe {
-            buffer.write(InstanceCounted::new(counter));
+            assert!(!p.is_null());
+            // SAFETY: The pointer is non-null and points to a valid initialized object.
+            p.write(InstanceCounted::new());
+
+            // SAFETY: The pointer is allocated by `new`, and the pointee is initialized.
+            <cc_std::std::unique_ptr<InstanceCounted>>::new(p)
         }
-        buffer
     }
 }
 
 #[gtest]
 fn test_unique_ptr_can_be_dropped() {
-    let counter = Rc::new(RefCell::new(0i32));
-    let up = unsafe {
-        cc_std::std::unique_ptr::new(InstanceCounted::new_from_cpp_allocator(counter.clone()))
-    };
-    assert_eq!(*counter.borrow(), 1);
+    let up = InstanceCounted::new_unique_ptr();
+    assert_eq!(INSTANCE_COUNTER.load(Ordering::Acquire), 1);
     drop(up);
-    assert_eq!(*counter.borrow(), 0);
+    assert_eq!(INSTANCE_COUNTER.load(Ordering::Acquire), 0);
 }
 
 #[gtest]
 fn test_unique_ptr_get_returns_non_owned_pointer() {
-    let counter = Rc::new(RefCell::new(0i32));
-    let pointer = InstanceCounted::new_from_cpp_allocator(counter.clone());
-    let up = unsafe { cc_std::std::unique_ptr::new(pointer) };
-    assert_eq!(up.get(), pointer);
-    assert_eq!(*counter.borrow(), 1);
+    let up = InstanceCounted::new_unique_ptr();
+    assert_eq!(up.get(), up.get());
+    assert_eq!(INSTANCE_COUNTER.load(Ordering::Acquire), 1);
 }
 
 #[gtest]
-fn test_unique_ptr_get_returns_owned_pointer() {
-    let counter = Rc::new(RefCell::new(0i32));
-    let pointer = InstanceCounted::new_from_cpp_allocator(counter.clone());
-    let mut up = unsafe { cc_std::std::unique_ptr::new(pointer) };
+fn test_unique_ptr_release_returns_owned_pointer() {
+    let mut up = InstanceCounted::new_unique_ptr();
+    let pointer = up.get();
     let owned_pointer = up.release();
     assert_eq!(owned_pointer, pointer);
-    assert_eq!(*counter.borrow(), 1);
+    assert_eq!(INSTANCE_COUNTER.load(Ordering::Acquire), 1);
 
     // Consume the pointer.
     let up = unsafe { cc_std::std::unique_ptr::new(owned_pointer) };
     drop(up);
-    assert_eq!(*counter.borrow(), 0);
+    assert_eq!(INSTANCE_COUNTER.load(Ordering::Acquire), 0);
+}
+
+/// Tests the behavior when a unique_ptr created in C++ is destroyed in Rust.
+///
+/// For example, ASan can flag any poor behavior here.
+#[gtest]
+fn test_unique_ptr_destroyed_in_rust() {
+    let up = test_helpers::unique_ptr_test::create_unique_ptr();
+    drop(up);
+}
+
+/// Tests the behavior when a unique_ptr created in Rust is destroyed in C++.
+///
+/// For example, ASan can flag any poor behavior here.
+#[gtest]
+fn test_unique_ptr_destroyed_in_cpp() {
+    let mut up = test_helpers::unique_ptr_test::create_unique_ptr();
+    let up = unsafe { cc_std::std::unique_ptr::new(up.release()) };
+    test_helpers::unique_ptr_test::destroy_unique_ptr(up);
 }
---
+++ support/cc_std/test/unique_ptr/test_helpers.h
@@ -0,0 +1,18 @@
+// Part of the Crubit project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#ifndef THIRD_PARTY_CRUBIT_SUPPORT_CC_STD_TEST_UNIQUE_PTR_TEST_HELPERS_H_
+#define THIRD_PARTY_CRUBIT_SUPPORT_CC_STD_TEST_UNIQUE_PTR_TEST_HELPERS_H_
+
+#include <memory>
+
+namespace unique_ptr_test {
+
+inline std::unique_ptr<int> create_unique_ptr() {
+  return std::make_unique<int>(1);
+}
+inline void destroy_unique_ptr(std::unique_ptr<int>) {}
+
+}  // namespace unique_ptr_test
+
+#endif  // THIRD_PARTY_CRUBIT_SUPPORT_CC_STD_TEST_UNIQUE_PTR_TEST_HELPERS_H_
