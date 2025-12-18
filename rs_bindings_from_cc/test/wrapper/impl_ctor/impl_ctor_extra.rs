53f1388e0c7a966429dbe8f10abfc7dc52cd28e9
Taylor Cramer <cramertj@google.com>
2025-12-18 15:57:09 -0800

    Remove TODOs that were obsoleted by constructor lifetime inference
    PiperOrigin-RevId: 846447725

--- rs_bindings_from_cc/test/wrapper/impl_ctor/impl_ctor_extra.rs
+++ rs_bindings_from_cc/test/wrapper/impl_ctor/impl_ctor_extra.rs
@@ -2,25 +2,24 @@
 // Exceptions. See /LICENSE for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 extern crate alloc;
-use ::ctor::Emplace;
+
 use alloc::boxed::Box;
 use core::pin::Pin;
+use ctor::{mov, CtorNew, Emplace};
 
 /// Wraps `Create()` with a `Box`-based API.
 pub fn create() -> Pin<Box<crate::Nontrivial>> {
     Box::emplace(crate::Create())
 }
 
-// TODO(b/411467353): don't support move constructors yet, so can't do this:
-// use ::ctor::{CtorNew, mov};
-// /// Wraps `Read()` with a `Box`-based API.
-// pub fn read(x: Pin<Box<crate::Nontrivial>>) -> core::ffi::c_int {
-//   // The ctor API makes everything more explicit than C++ or Rust:
-//   // 1. we must explicitly reborrow the pointee, using .as_mut()
-//   // 2. we must cast to an rvalue reference, using `mov!()`
-//   // 3. we must call the constructor, using `CtorNew`.
-//   //
-//   // (If we were to materialize the temporary, we'd need a call to
-//   // `emplace!()` or Box::emplace, too!)
-//   crate::Read(crate::Nontrivial::ctor_new(mov!(x.as_mut())))
-// }
+/// Wraps `Read()` with a `Box`-based API.
+pub fn read(mut x: Pin<Box<crate::Nontrivial>>) -> core::ffi::c_int {
+    // The ctor API makes everything more explicit than C++ or Rust:
+    // 1. we must explicitly reborrow the pointee, using .as_mut()
+    // 2. we must cast to an rvalue reference, using `mov!()`
+    // 3. we must call the constructor, using `CtorNew`.
+    //
+    // (If we were to materialize the temporary, we'd need a call to
+    // `emplace!()` or Box::emplace, too!)
+    crate::Read(crate::Nontrivial::ctor_new(mov!(x.as_mut())))
+}
--- rs_bindings_from_cc/test/wrapper/impl_ctor/test.rs
+++ rs_bindings_from_cc/test/wrapper/impl_ctor/test.rs
@@ -2,20 +2,19 @@
 // Exceptions. See /LICENSE for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-use googletest::prelude::*;
+use googletest::{expect_eq, gtest};
+use impl_ctor::{create, read};
 
 #[gtest]
 fn test_wrapped_library() {
-    let mut x = impl_ctor::create();
-    assert_eq!(x.value, 42);
+    let mut x = create();
+    expect_eq!(x.value, 42);
 
     // Writing a public field requires pin-projecting to the field,
     // but since `c_int` is `Unpin`, the field itself requires no
     // more pin stuff.
     *x.as_mut().project_pin().value = 100;
 
-    assert_eq!(x.value, 100);
-
-    // TODO(b/411467353): don't support move constructors yet, so can't do this
-    // assert_eq!(wrapped_library::read(value), 100);
+    expect_eq!(x.value, 100);
+    expect_eq!(read(x), 100);
 }
