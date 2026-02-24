3fcf4d84af118a78a2ecc0f9d6f8e5bf1645d154
Ethan Smith <ethansmith@google.com>
2026-02-24 10:31:16 -0800

    Cargo subcommand for cpp_api_from_rust.
    Subcommand is invoked as `cargo cpp_api_from_rust` and will do three
    things in the corresponding Rust project:
    
    1) Build the project, producing an `.rlib`
    2) Run `cc_bindings_from_rs` on the corresponding `.rlib`
    3) Compile the generated rust impl into a static lib
    
    It generates the `.h` (and soon `.cc`) files but does not compile them.
    The subcommand still needs it's own command line flag interface, so a
    user can specify flags one might pass to cargo and we correctly route it
    to the underlying `cargo build` call.
    
    PiperOrigin-RevId: 874681983

---
+++ cc_bindings_from_rs/cargo-cpp_api_from_rust.rs
@@ -0,0 +1,162 @@
+// Part of the Crubit project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+//! This cargo subcommand generates C++ bindings for a given Cargo project.
+//! Expectation is that it is invoked within a cargo project directory via:
+//!
+//! ```shell
+//! $ cargo cpp_api_from_rust \
+//!   --<cargo-flag> \
+//!   ...
+//! ```
+//! outputting a static library and header file that contain the generated C++ bindings.
+//! These outputs can be consumed by a C++ build system (e.g. CMake) to compile Rust into a C++
+//! codebase.
+//!
+//! The cargo project being built must specify a single library target in the root manifest.
+//! Generating bindings for multiple packages, such as in a workspace, is not supported. The
+//! subcommand will build your target Rust crate, failing with the compiler error if it fails to
+//! build.
+
+#![feature(rustc_private)]
+#![feature(never_type)]
+
+use arc_anyhow::{anyhow, bail, Error, Result};
+use cargo_metadata::{Message, MetadataCommand};
+use cmdline::Cmdline;
+
+use std::env;
+use std::ffi;
+use std::process::{Command, Stdio};
+
+// TODO(b/448731652): This should support passthrough for arbitrary cargo commandline flags that
+// one might want to specify. This is important both for building target package initially, and
+// building the final static lib at the end.
+fn main() -> Result<()> {
+    //! This subcommand executes three subtasks to generate bindings:
+    //!
+    //! 1. Build the target Rust crate, outputting an rlib file.
+    //! 2. Run `cpp_api_from_rust` on the rlib file, outputting C++, rs, and h files.
+    //! 3. Build a static lib out of the rs file generated in step 2.
+    let metadata = MetadataCommand::new().exec().unwrap();
+
+    let root = metadata.root_package().ok_or_else(|| anyhow!("Failed to find root package"))?;
+
+    let edition = root.edition;
+    let source_crate_name = root.name.as_ref();
+
+    let mut args = vec![
+        // Make up a binary name for our call. This will get removed by Cmdline, so it doesn't matter
+        // but it must be here otherwise a real cmdline arg will get ate.
+        "cpp_api_from_rust".to_string(),
+        format!("--source-crate-name={}", source_crate_name),
+        "--crubit-support-path-format=<{header}>".to_string(),
+        "--enable-rmeta-interface".to_string(),
+    ];
+
+    // Step 1: Build the target Rust crate, outputting an rlib file.
+    // TODO: Include cargo flags in this from our own args.
+    let mut command = Command::new(cargo_bin())
+        //We build, instead of check, because we need the rlib output to compile out final
+        // staticlib.
+        .args(&["build", "--message-format=json"])
+        .stdout(Stdio::piped())
+        .spawn()
+        .map_err(|err| anyhow!("Failed to spawn cargo: {}", err))?;
+
+    let mut profile_dir = None;
+    let mut source_crate_rlib = None;
+    let reader = std::io::BufReader::new(
+        command.stdout.take().ok_or_else(|| anyhow!("Failed to open cargo stdout"))?,
+    );
+    for message in cargo_metadata::Message::parse_stream(reader) {
+        let message = message.map_err(|err| anyhow!("Failed to parse cargo message: {}", err))?;
+        let Message::CompilerArtifact(artifact) = message else {
+            // We only care about compiler artifacts. Skip other messages, but display any text
+            // output from the build.
+            let output = match message {
+                Message::TextLine(line) => line,
+                Message::CompilerMessage(msg) => format!("{}", msg),
+                _ => continue,
+            };
+            eprintln!("{}", output);
+            continue;
+        };
+        let find_metadata_file = artifact.filenames.iter().find(|filename| {
+            filename.extension().is_some_and(|ext| ext == "rmeta" || ext == "rlib")
+        });
+        let Some(filename) = find_metadata_file else {
+            continue;
+        };
+        let name = artifact.target.name;
+        if name == source_crate_name {
+            profile_dir = filename
+                .strip_prefix(&metadata.target_directory)
+                .ok()
+                .and_then(|path| path.components().next())
+                .map(|component| component.as_str().to_owned());
+            source_crate_rlib = artifact
+                .filenames
+                .iter()
+                .find(|filename| filename.extension().is_some_and(|ext| ext == "rlib"))
+                .map(|filename| filename.as_str().to_owned());
+        }
+        args.push(format!("--extern={}={}", name, filename));
+    }
+
+    let (Some(source_crate_rlib), Some(profile_dir)) = (source_crate_rlib, profile_dir) else {
+        bail!(
+            "Failed to find profile directory or rlib for target crate. Most likely \
+        cause is cargo failed to build the specified target."
+        );
+    };
+
+    let out_dir = metadata.target_directory.join(profile_dir);
+    args.extend([
+        format!("--h-out={}", out_dir.join(format!("{}.h", source_crate_name))),
+        format!("--rs-out={}", out_dir.join(format!("{}_cc_api_impl.rs", source_crate_name))),
+        format!("--cpp-out={}", out_dir.join(format!("{}.cpp", source_crate_name))),
+        format!("-Ldependency={}", out_dir.join("deps")),
+    ]);
+
+    // Wait on `cargo check` to finish to ensure all our files are generated.
+    command.wait().map_err(|err| anyhow!("Failed to wait for cargo: {}", err))?;
+
+    // Step 2: Run `cpp_api_from_rust` on the rlib file, outputting C++, rs, and h files.
+    Cmdline::new(&args)
+        .map_err(|err| err.into())
+        .and_then(|cmdline| cpp_api_from_rust_lib::run_with_cmdline_args(&cmdline))
+        .map_err(|err: Error| match err.downcast_ref::<clap::Error>() {
+            // Explicitly call `clap::Error::exit`, because 1) it results in *colored* output and
+            // 2) it uses a zero exit code for specific "errors" (e.g. for `--help` output).
+            Some(clap_err) => {
+                let _: ! = clap_err.exit();
+            }
+
+            // Return `err` from `main`.  This will print the error message (no color codes
+            // though) and terminate the process with a non-zero exit code.
+            None => err,
+        })?;
+
+    // Step 3: Build a static lib out of the rs file generated in step 2.
+    let rustc_args = vec![
+        "rustc".to_string(),
+        out_dir.join(format!("{}_cc_api_impl.rs", source_crate_name)).to_string(),
+        format!("--edition={}", edition),
+        format!("--crate-name={}_cc_api", source_crate_name),
+        "--crate-type=staticlib".to_string(),
+        format!("--extern={}={}", source_crate_name, source_crate_rlib),
+        format!("-Ldependency={}", out_dir.join("deps")),
+        "-o".to_string(),
+        out_dir.join(format!("lib{}.a", source_crate_name)).to_string(),
+    ];
+    cpp_api_from_rust_lib::run_rustc(&rustc_args);
+
+    Ok(())
+}
+
+fn cargo_bin() -> &'static ffi::OsStr {
+    static CARGO_BIN: std::sync::OnceLock<ffi::OsString> = std::sync::OnceLock::new();
+    CARGO_BIN.get_or_init(|| env::var_os("CARGO").unwrap_or_else(|| "cargo".into()))
+}
