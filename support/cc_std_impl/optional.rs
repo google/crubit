9f3c5f1da93854cbd6c2ec9726a34ef35af5b185
Googler <no-reply@google.com>
2026-07-30 10:03:25 -0700

    Remove bug stale link
    The bug has been fixed. We still need to add the annotation, but that work isn't blocked anymore because `crubit_annotate::layout_compatible` supports generics now.
    
    PiperOrigin-RevId: 956589520

--- support/cc_std_impl/optional.rs
+++ support/cc_std_impl/optional.rs
@@ -7,8 +7,7 @@ use core::mem::MaybeUninit;
 /// C++ std::optional<T>.
 // This is the layout that libc++ uses, which is guaranteed to be stable:
 // https://github.com/llvm/llvm-project/blob/main/libc/src/__support/CPP/optional.h#L34-L39
-// TODO(b/497904084): Use crubit_annotate::layout_compatible once generics are supported so that
-// cc_bindings_from_rs can map this type back to C++ `std::optional<T>`.
+// TODO(b/493911621): Add crubit_annotate::layout_compatible annotation.
 #[allow(non_snake_case)]
 #[repr(C)]
 pub struct optional<T> {
