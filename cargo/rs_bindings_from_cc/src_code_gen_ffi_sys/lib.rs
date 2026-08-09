4e7f11afaabde59afeb7703ef967e4387aab782d
Lukasz Anforowicz <lukasza@google.com>
2026-08-09 10:21:32 -0700

    Add cargo/rs_bindings_from_cc/ directories to GitHub repo.
    PiperOrigin-RevId: 961784613

---
+++ cargo/rs_bindings_from_cc/src_code_gen_ffi_sys/lib.rs
@@ -0,0 +1,8 @@
+// Part of the Crubit project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+// Automatically @generated lib.rs for the cc_library src_code_gen_ffi.
+
+extern crate cc_ffi_types_sys;
+extern crate profiling_sys;
---
+++ cargo/rs_bindings_from_cc/src_code_gen_sys/lib.rs
@@ -0,0 +1,10 @@
+// Part of the Crubit project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+// Automatically @generated lib.rs for the cc_library src_code_gen.
+
+extern crate cc_api_ffi;
+extern crate cc_ir_sys;
+extern crate profiling_sys;
+extern crate src_code_gen_ffi_sys;
