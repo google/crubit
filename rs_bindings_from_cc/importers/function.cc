e5b059c7b3271a5f400b0005887b1be94c5a1e30
Kezia Rijadi <rrijadi@google.com>
2026-08-09 13:55:54 -0700

    Replace enum IR types in ir.h/ir.cc with ir_proto* equivalents.
    PiperOrigin-RevId: 961831760

--- rs_bindings_from_cc/importers/function.cc
+++ rs_bindings_from_cc/importers/function.cc
@@ -83,26 +83,26 @@ SafetyAnnotation GetCrubitSafetyAnnotation(const clang::Decl& decl,
       GetAnnotateAttrArgs(decl, "crubit_override_unsafe");
   if (!maybe_args.ok()) {
     errors.AddStatus(std::move(maybe_args).status());
-    return SafetyAnnotation::kUnannotated;
+    return SafetyAnnotation::SAFETY_ANNOTATION_UNANNOTATED;
   }
   if (!maybe_args->has_value()) {
-    return SafetyAnnotation::kUnannotated;
+    return SafetyAnnotation::SAFETY_ANNOTATION_UNANNOTATED;
   }
   const AnnotateArgs& args = **maybe_args;
   if (args.size() != 1) {
     errors.AddStatus(absl::InvalidArgumentError(
         "`crubit_override_unsafe` annotation must have exactly one argument"));
-    return SafetyAnnotation::kUnannotated;
+    return SafetyAnnotation::SAFETY_ANNOTATION_UNANNOTATED;
   }
   absl::StatusOr<bool> is_unsafe =
       GetExprAsBool(*args[0], decl.getASTContext());
   if (!is_unsafe.ok()) {
     errors.AddStatus(std::move(is_unsafe).status());
-    return SafetyAnnotation::kUnannotated;
+    return SafetyAnnotation::SAFETY_ANNOTATION_UNANNOTATED;
   } else if (*is_unsafe) {
-    return SafetyAnnotation::kUnsafe;
+    return SafetyAnnotation::SAFETY_ANNOTATION_UNSAFE;
   } else {
-    return SafetyAnnotation::kDisableUnsafe;
+    return SafetyAnnotation::SAFETY_ANNOTATION_DISABLE_UNSAFE;
   }
 }
 
@@ -120,12 +120,12 @@ void CollectUnsafeAttr(const clang::Decl& decl, Errors& errors, bool is_safe,
                                    attrs.begin()->getSpelling()));
     return;
   }
-  safety = SafetyAnnotation::kUnsafe;
+  safety = SafetyAnnotation::SAFETY_ANNOTATION_UNSAFE;
 }
 
 SafetyAnnotation GetSafetyAnnotation(const clang::Decl& decl, Errors& errors) {
   SafetyAnnotation safety = GetCrubitSafetyAnnotation(decl, errors);
-  bool is_safe = safety == SafetyAnnotation::kDisableUnsafe;
+  bool is_safe = safety == SafetyAnnotation::SAFETY_ANNOTATION_DISABLE_UNSAFE;
   CollectUnsafeAttr<clang::UnsafeBufferUsageAttr>(decl, errors, is_safe,
                                                   safety);
 
@@ -153,8 +153,8 @@ void ApplyRefQualifierToThisPointer(CcType& this_param_type,
       ref_qualifier_kind == clang::RefQualifierKind::RQ_RValue) {
     // It was just a non null pointer, but because of the rvalue ref
     // qualification, it should be an rvalue reference.
-    CHECK(pointer->kind == PointerTypeKind::kNonNull);
-    pointer->kind = PointerTypeKind::kRValueRef;
+    CHECK(pointer->kind == PointerTypeKind::NON_NULL);
+    pointer->kind = PointerTypeKind::R_VALUE_REF;
   }
 }
 
@@ -1066,7 +1066,7 @@ std::unique_ptr<ir_proto::Item> FunctionDeclImporter::Import(
   func->set_has_c_calling_convention(has_c_calling_convention);
   func->set_is_member_or_descendant_of_class_template(
       is_member_or_descendant_of_class_template);
-  func->set_safety_annotation(ToFlatProto(safety_annotation));
+  func->set_safety_annotation(safety_annotation);
   func->set_source_loc(
       ictx_.ConvertSourceLocation(function_decl->getBeginLoc(), &name_info));
   func->set_id(ictx_.GenerateItemId(function_decl).value());
