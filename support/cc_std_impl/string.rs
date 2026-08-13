a87c43ae6d67ca00a88a52a5cd56444d4209e8d3
Lukasz Anforowicz <lukasza@google.com>
2026-08-13 00:01:30 -0700

    Mostly internal-only changes.
    PiperOrigin-RevId: 963890617

--- support/cc_std_impl/string.rs
+++ support/cc_std_impl/string.rs
@@ -369,12 +369,14 @@ pub unsafe extern "C" fn cpp_string_to_rust_string(input: *mut c_void, output: *
 /// assert_eq!(my_string.to_str().unwrap(), "Hello, C++!");
 /// ```
 #[crubit_annotate::cpp_layout_equivalent(cpp_type = "::std::string", include_path = "<string>")]
-#[repr(C, align(8))]
+#[cfg_attr(target_pointer_width = "64", repr(C, align(8)))]
+#[cfg_attr(target_pointer_width = "32", repr(C, align(4)))]
 #[allow(non_camel_case_types)]
 pub struct new_string {
     _opaque: core::cfg_select! {
         any(target_arch = "x86_64", target_arch = "aarch64") => [u8; 24],
-        _ => compile_error!("new_string is only supported on x86_64 and aarch64"),
+        any(target_arch = "arm", target_arch = "x86") => [u8; 12],
+        _ => compile_error!("new_string is only supported on x86_64, aarch64, arm, and x86"),
     },
     _pinned: core::marker::PhantomPinned,
 }
